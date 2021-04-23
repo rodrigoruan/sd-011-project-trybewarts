ebdf5c9f543341f2b515006d10190bc4789b90b6
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-23 19:04:47 -0300

    Ajuste Requisito 3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const passUser = document.getElementById('password');
 const loginBtn = document.querySelector('#header__form--botao');
 
 function verifyLogin() {
-  if (loginUser.value !== 'tryber@betrybe.com' || passUser.value !== '123456') {
+  if (loginUser.value !== 'tryber@betrybe.com' && passUser.value !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
