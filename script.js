67441b92608463071a86c86438a79ad57aa40091
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-23 17:32:43 -0300

    requisito 17 e 18 feitos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const loginEmailInput = document.getElementById('login-email-input');
 const loginPasswordInput = document.getElementById('login-password-input');
 const loginButton = document.getElementById('login-button');
+const submitButton = document.getElementById('submit-btn');
+const checkAgreement = document.getElementById('agreement');
 
 function login() {
   const email = loginEmailInput.value;
@@ -13,3 +15,14 @@ function login() {
 }
 
 loginButton.addEventListener('click', login);
+
+function disableSubmitButton() {
+  checkAgreement.addEventListener('click', () => {
+    if (submitButton.disabled) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  });
+}
+disableSubmitButton();
