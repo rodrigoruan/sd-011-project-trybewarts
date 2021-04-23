01bf40f3c4e76873744d3a769383428240593bb8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-23 16:45:45 -0300

    Finaliza requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+/** Verifica se login */
+const loginButton = document.querySelector('#login-btn');
+const inputUser = document.querySelector('#login');
+const inputPass = document.querySelector('#pass');
+
+loginButton.addEventListener('click', () => {
+  if (inputUser.value !== 'tryber@teste.com' && inputPass.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+});
