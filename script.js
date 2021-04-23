a96d783317a2645ea0b5eb1e95c0f2682396269e
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-23 17:06:13 -0300

    Questão 16

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const loginInput = document.getElementById('id');
 const senhaInput = document.getElementById('password');
 const button = document.getElementById('submit-button');
-
-console.log(loginInput.value);
+const checkBox = document.getElementById('agreement');
 
 button.addEventListener('click', () => {
   if (loginInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+checkBox.addEventListener('click', () => {
+  const submitBtn = document.getElementById('submit-btn');
+  if (checkBox.checked) {
+    console.log(checkBox.checked);
+    submitBtn.removeAttribute('disabled');
   }
 });
