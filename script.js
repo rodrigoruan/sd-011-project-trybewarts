f2cc30ac06d0ad6c5a8eb212ec4fcfdd314f0785
rubia braga <rubiaemilly29@gmail.com>
2021-04-23 18:15:54 -0300

    modelo lint atualizado

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 /* Requisito 3 */
 document.getElementById('loginButton').addEventListener('click', () => {
   const email = document.getElementById('email').value;
-	const senha = document.getElementById('senha').value;
+  const senha = document.getElementById('senha').value;
   const trybeEmail = 'tryber@teste.com';
   const trybeSenha = '123456';
   if (email === trybeEmail && senha === trybeSenha) {
-		alert('Ola, Tryber!');
+    alert('Ola, Tryber!');
   } else {
-	  alert('Login ou Senha invalidos.');
+    alert('Login ou Senha invalidos.');
   }
 });
-
