e2c8dd4864d355e8410459f5780161e63954e219
Víctor Faria <vitilevi@hotmail.com>
2021-04-23 17:11:36 -0300

    Feito requisitos 1, 2, 3 e 4

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-function addEventListeners() {
+const loginButton = document.getElementById('login');
 
+function checkForLogin() {
+  const loginValue = document.getElementById('login-value').value;
+  const passwordValue = document.getElementById('password-value').value;
+  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+function addEventListeners() {
+  loginButton.addEventListener('click', checkForLogin);
 }
 
 window.onload = () => {
   addEventListeners();
-}
+};
