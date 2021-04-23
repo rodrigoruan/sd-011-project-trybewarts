dc8d61d1a5d63627d00e41e7978f725a6a46a792
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-23 18:00:27 -0300

    Correçoes lint

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const loginButton = document.querySelector('#login-button');
 
-loginButton.addEventListener('click', function() {
-  const user = document.forms['trybewarts-login'].user;
-  const password = document.forms['trybewarts-login'].password;
-  if (user.value === 'tryber@teste.com' && password.value === '123456') {
+loginButton.addEventListener('click', function () {
+  const trybewartsLogin = document.forms['trybewarts-login'];
+  if (trybewartsLogin.user.value === 'tryber@teste.com' && trybewartsLogin.password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos');
