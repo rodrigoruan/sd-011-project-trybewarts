81cccc43bdd57e4bb5704a10f9b5ca30a5c25c0e
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-04-23 16:11:34 -0300

    Requisito 3 completado

---
+++ script.js
@@ -0,0 +1,16 @@
+const loginInput = document.getElementById('loginInput');
+const senhaInput = document.getElementById('senhaInput');
+const submitLoginBttn = document.getElementById('login-button');
+
+
+
+
+submitLoginBttn.addEventListener('click', () => {
+  const loginInputVal = loginInput.value;
+  const senhaInputVal = senhaInput.value;
+  if (loginInputVal !== 'tryber@teste.com' || senhaInputVal !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else if (loginInputVal === 'tryber@teste.com' && senhaInputVal === '123456') {
+    alert('Olá, Tryber!');
+  }
+});
