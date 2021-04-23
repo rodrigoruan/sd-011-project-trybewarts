526d321a26e5064731fd5a127ca08e710d3883e0
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-23 16:53:39 -0300

    Ajuste input e button

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const submit = document.getElementById("submit"); // variável recebendo o botão submit
 const pass = document.getElementById("pass"); // variavel recebendo o password
 
-submit.addEventListener("click", function checkEmail() {
+submit.addEventListener("click", () => {
   const email = document.getElementById("email"); // Variavel recebendo o email
   let insertedEmail = email.value;
   window.alert(email.value); //default = email 'tryber@teste.com' senha '123456'
 });
+
