cefb088a50ae2ff8fbb13015cf8f6b7caf62a0ff
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-23 17:00:33 -0300

    [ADD] ex3 - duvidas duvidosas

---
+++ script.js
@@ -0,0 +1,18 @@
+// -Declaração de constantes
+const passwordInput = document.querySelector('#password');
+const buttonLogin = document.querySelector('#button');
+
+function emailChecker() {
+  const loginInput = document.querySelector('#email');
+  for ( let index = 0; index < texto.length; index += 1){
+    console.log('a');
+  }
+
+//   let test = loginInput.innerText.split('@');
+
+//   if () {
+//     console.log('a')
+//   }
+}
+
+buttonLogin.addEventListener('click', emailChecker);
