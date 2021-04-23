384e2c6a3823c6262bc8119ff0aa41f0bb6033e5
Jessica Arruda <sicarruda@gmail.com>
2021-04-23 16:44:34 -0300

    ajuste das aspas para o padrao EsLint

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const password = document.querySelector("#password");
 const buttonSubmit = document.querySelector("#submit");
 
 function loginValidation(imputPassword, imputLogin) {
-  if (imputPassword !== 123456 && imputLogin !== 'tryber@teste.com') {
-    return alert('Login ou senha inválidos');
+  if (imputPassword !== 123456 && imputLogin !== "tryber@teste.com") {
+    return alert("Login ou senha inválidos");
   }
-  alert('Olá Tryber');
+  alert("Olá Tryber");
 }
 
 function loginSubmit() {
   loginValidation(password.value, login.value);
 }
 
-buttonSubmit.addEventListener('click', loginSubmit);
-
+buttonSubmit.addEventListener("click", loginSubmit);
