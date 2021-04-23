9a2ef174adca95c6a7e83480a778c4963053c79f
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-04-23 17:49:59 -0300

    Adiciona exercicio 14

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const login = document.querySelector('#login');
 const password = document.querySelector('#password');
 const join = document.querySelector('#join');
+const feedback = document.querySelector('.feedback');
+
+for (let index = 1; index <= 10; index += 1) {
+  const label = document.createElement('label');
+  const radio = document.createElement('input');
+  label.innerText = index;
+  radio.type = 'radio';
+  radio.name = 'rate';
+  radio.value = index;
+  label.appendChild(radio);
+  feedback.appendChild(label);
+}
 
 join.addEventListener('click', (e) => {
   e.preventDefault();
