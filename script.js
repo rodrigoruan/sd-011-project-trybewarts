7491396ac3cf6c1f3ed476f04a3c6b0d643d8db8
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-04-23 19:29:46 -0300

    Ultimas atualizacoes antes de encerrar o dia

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const buttonLogin = document.getElementById('button-login');
 const inputLogin = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
+const buttonForm = document.getElementById('submit-btn').disabled = true;
+const agreementSelect = document.getElementById('agreement');
 
 function validLogin() {
   if (inputLogin.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -14,4 +16,15 @@ function login() {
   buttonLogin.addEventListener('click', validLogin);
 }
 
+function validForm() {
+  // if (agreementSelect.value === 'on') {
+  //   buttonForm.disabled=true;
+  //   console.log(agreementSelect.value);
+  // } else {
+  //   buttonForm.disabled=false;
+  //   console.log(agreementSelect.value);
+  // }
+}
+
 login();
+validForm();
