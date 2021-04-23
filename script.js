38a71d76ce44ba5f26fd344dab740a4f8915e26a
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-23 16:26:29 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+// function clickLogin() {
+
+// }
+
+
+// function validateLogin() {
+//   const loginButton = document.getElementById('login-button');
+//   loginButton.addEventListener('click', clickLogin);
+// }
+
+// window.onload = function load() {
+  
+// };
