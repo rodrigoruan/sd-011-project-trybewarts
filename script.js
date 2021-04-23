b4d17ab38c983ff8db47c0429a6c653a31180d71
Rodolfo <rodolforrcont@gmail.com>
2021-04-23 16:18:32 -0300

    requisito 3 finalizado

---
+++ script.js
@@ -0,0 +1,13 @@
+function login(event) {
+  const usuario = document.getElementsByName('Login')[0].value;
+  const senha = document.getElementsByName('Senha')[0].value;
+  if (usuario === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos');
+  }
+};
+
+const button = document.getElementsByClassName('btn-submit');
+
+button.addEventListener('click', login)
