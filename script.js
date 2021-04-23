e8c14cb6217a6e27dbdb5d1c3d0fb1d406f3f19d
Marco Galindo <marco1408@gmail.com>
2021-04-23 17:00:45 -0300

    teste

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function login(event) {
+function login() {
   const usuario = document.getElementsByName('Login')[0].value;
   const senha = document.getElementsByName('Senha')[0].value;
+  console.log(usuario);
   if (usuario === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos');
   }
-};
+}
