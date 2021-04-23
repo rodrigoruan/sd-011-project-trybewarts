7fde00620cedd66cdc26437065ff830e1425dab0
Dario Junior <dario.junior.rs@gmail.com>
2021-04-23 19:08:35 -0300

    resolve requisito 3

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// function clickLogin() {
-//   console.log('teste');
-// }
+function clickLogin() {
+  const email = event.path[2][0].value;
+  const password = event.path[2][1].value;
 
-// function validateLogin() {
+  email === 'tryber@teste.com' && password === '123456'
+    ? window.alert('Olá, Tryber!')
+    : window.alert('email ou senha inválida');
+};
+
+// function validateLogin() { 
 //   const loginButton = document.getElementById('login-button');
 //   loginButton.addEventListener('click', clickLogin);
 // }
