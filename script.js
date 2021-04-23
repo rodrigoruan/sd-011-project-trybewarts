5f69f285c3220f2dc6fe367cba5d82baf4b8bcd8
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-23 17:19:36 -0300

    Entendo Lint

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Exercício 3
 const loginButton = document.getElementById('button-logar');
-loginButton.addEventListener('click', function* go() {
-  const valueLogin = document.getElementById('login').value;
-  const valueSenha = document.getElementById('senha').value;
+const valueLogin = document.getElementById('login').value;
+const valueSenha = document.getElementById('senha').value;
+
+loginButton.addEventListener('click', () => {
   if (valueLogin !== 'tryber@teste.com' || valueSenha !== '123456') {
     alert('Login ou senha inválidos');
   } else {
     alert('Olá, Tryber!');
   }
-  yield;
 });
