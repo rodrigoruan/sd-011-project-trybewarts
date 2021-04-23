dd9a2efd00e846d8c154ab62d22b2266aa52dc06
Laura Gusmao <laurargus@gmail.com>
2021-04-23 17:54:56 -0300

    Ateracoes requisito 3

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ function validateLogin() {
   const pass = document.getElementById('input-password');
   if (login.value === 'tryber@teste.com' && pass.value === '123456') {
     return true;
-  }  
+  }
 }
-
 
 function loginChecked() {
   const loginButton = document.getElementById('button-header');
   loginButton.addEventListener('click', function () {
     if (!validateLogin) {
-     alert('Login ou senha inválidos.'); 
+     alert('Login ou senha inválidos.');
     }
     alert('Olá, Tryber!');
   });
-};
+}
 
 loginChecked();
