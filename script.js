6430d2dba59b5dc2cf4837597b3f587d52cdf445
Tales Coelho <tales-coelho@live.com>
2021-04-23 16:00:10 -0300

    efetuando o login do site

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const headerForm = document.querySelector('.header-form');
-console.log(headerForm);
-
+const headerForm = document.querySelector('.trybewarts-login');
 const loginSubmit = document.getElementById('trybewarts-login-submit');
 
 loginSubmit.addEventListener('click', (event) => {
   event.preventDefault();
-  alert('aeee');
+  if (headerForm.elements[0].value === 'tryber@teste.com'
+  && headerForm.elements[1].value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 });
+
+// tryber@teste.com
+// 123456
