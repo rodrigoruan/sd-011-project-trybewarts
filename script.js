9121c9b3582f1d6b76ac094a0b130bf81326ce0c
VictorS <czpask@gmail.com>
2021-04-23 17:37:21 -0300

    Requisitos 1 ao 20

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ const submitButton = document.getElementById('submit-btn');
 const loginButton = document.querySelector('#loginBtn');
 const loginPlace = document.querySelector('#loginValue');
 const passwordPlace = document.querySelector('#senhaValue');
+const textPlace = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
 
 function agreementCheck() {
-	if(checkboxAgreement.checked === false){
-		submitButton.disabled = true;
-	} else {
-		submitButton.disabled = false;
-	}
+  if (checkboxAgreement.checked === false) {
+    submitButton.disabled = true;
+  } else {
+    submitButton.disabled = false;
+  }
 }
 
 checkboxAgreement.addEventListener('click', agreementCheck);
 
-loginButton.addEventListener('click', function () {
+loginButton.addEventListener('click', () => {
   if (loginPlace.value === 'tryber@teste.com' && passwordPlace.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 });
+
+textPlace.addEventListener('keyup', () => {
+  const textSize = textPlace.value.length;
+  const count = 500 - textSize;
+  counter.innerHTML = count.toString();
+})
+
