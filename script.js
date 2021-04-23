97857f8dd04591cf04ead5f3335a2c5c9ef2d615
Michel Pereira <michelbecauro@gmail.com>
2021-04-23 18:08:11 -0300

    Rascunho requisito 3

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+function valida () {
+
+    let email = document.getElementById("trybewarts-login").;
+
+    if (email !== "trybe@gameil.com") {
+			
+    }
+
+	}
+
+	valida ();
+
+}
