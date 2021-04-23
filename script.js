e5d7e5b3cbd155498a8d3a961cb5bb370dbf96ab
nandovbr <nandovbr15@hotmail.com>
2021-04-23 17:03:54 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const loginButton = document.getElementById('login-button');
+const loginInput = document.getElementById('login');
+const passwordInput = document.getElementById('senha');
 
 loginButton.addEventListener('click', () => {
-  console.log('oi');
+  let loginValue = loginInput.value;
+  let passwordValue = passwordInput.value;
+
+  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
+    alert ('Olá, Tryber!');
+  } else {alert ('Login ou senha inválidos.')}
 });
