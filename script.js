e777bed0bf8946187bec351b249bfd5215ec9d25
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-04-23 17:42:27 -0300

    Questão 20

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const loginInput = document.getElementById('id');
 const senhaInput = document.getElementById('password');
 const button = document.getElementById('submit-button');
 const checkBox = document.getElementById('agreement');
+const textAreaValue = document.getElementById('textarea');
+const spanCount = document.getElementById('counter');
+
+function limitTextArea(valor) {
+  const quantidade = 500;
+  const total = valor.length;
+  console.log(total);
+  if (total <= quantidade) {
+    const rest = quantidade - total;
+    spanCount.innerHTML = rest;
+  } else {
+    textAreaValue.value = valor.substr(0, quantidade);
+  }
+}
+
+textAreaValue.addEventListener('input', () => {
+  limitTextArea(textAreaValue.value);
+});
 
 button.addEventListener('click', () => {
   if (loginInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
@@ -18,3 +36,7 @@ checkBox.addEventListener('click', () => {
     submitBtn.removeAttribute('disabled');
   }
 });
+
+const form = document.getElementsByClassName('trybewarts-login');
+
+form.get('name')
