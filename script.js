c284660afd6667bb6e675fd1a9f695d24c44d1ea
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-23 19:00:23 -0300

    checked e lint

--- script.js
+++ script.js
@@ -24,14 +24,12 @@ loginButton.addEventListener('click', () => {
   }
 });
 
-
 textPlace.addEventListener('keyup', () => {
   const textSize = textPlace.value.length;
   const count = 500 - textSize;
   counter.innerHTML = count.toString();
 });
 
-//21
 const nameInput = document.querySelector('#input-name');
 const lastNameInput = document.querySelector('#input-lastname');
 const emailInput = document.querySelector('#input-email');
@@ -39,20 +37,19 @@ const houseInput = document.querySelector('#house');
 const houseChoice = document.getElementsByClassName('family');
 const rateChoice = document.getElementsByClassName('radioRate');
 const wichContent = document.getElementsByClassName('subject');
-const radioRate = document.getElementsByClassName('radioRate');
-// const textPlace = document.querySelector('#textarea');
+
 function checkedRadio() {
-  for(let index = 0; index < houseChoice.length; index += 1){
-    if(houseChoice[index].checked === true){
-      return houseChoice[index]
+  for (let index = 0; index < houseChoice.length; index += 1) {
+    if (houseChoice[index].checked === true) {
+      return houseChoice[index];
     }
   }
 }
 
 function checkedSubject() {
-  const subjectsChoice = []
-  for(let index = 0; index < wichContent.length; index += 1){
-    if(wichContent[index].checked === true){
+  const subjectsChoice = [];
+  for (let index = 0; index < wichContent.length; index += 1) {
+    if (wichContent[index].checked === true) {
       const check = wichContent[index];
       subjectsChoice.push(check);
     }
@@ -61,36 +58,26 @@ function checkedSubject() {
 }
 
 function checkedRate() {
-  for(let index = 0; index < rateChoice.length; index += 1){
-    if(rateChoice[index].checked === true){
-      return rateChoice[index]
+  for (let index = 0; index < rateChoice.length; index += 1) {
+    if (rateChoice[index].checked === true) {
+      return rateChoice[index];
     }
   }
 }
 
-
-
-
 submitButton.addEventListener('click', (event) => {
-
-
   const checkedFunction = checkedRadio();
   const checkedRated = checkedRate();
   const checkedSubjects = checkedSubject();
-  
-
   event.preventDefault();
   console.log(nameInput.value);
   console.log(lastNameInput.value);
   console.log(emailInput.value);
   console.log(houseInput.value);
   console.log(checkedFunction.value);
-  for(let check of checkedSubjects){
+  for (const check of checkedSubjects) {
     console.log(check.value);
-  };
+  }
   console.log(checkedRated.value);
   console.log(textPlace.value);
-
-
 });
-
