2247c7312eedb092f8bdc4b12e86b2fd80c7052a
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-23 19:12:59 -0300

    Projeto em grupo aprovado

--- script.js
+++ script.js
@@ -6,14 +6,6 @@ const submit = document.querySelector('#submit-btn');
 const agree = document.querySelector('#agreement');
 const counter = document.querySelector('#counter');
 const textarea = document.querySelector('#textarea');
-// const clear = document.querySelector('.clear');
-// const inputName = document.querySelector('#input-name');
-// const inputName2 = document.querySelector('#input-lastname');
-// const email = document.querySelector('#input-email');
-// const casa = document.querySelector('#house');
-// const family = document.querySelector('input[name="family"]:checked');
-// const materias = document.querySelectorAll('[class="subject"]:checked');
-// const avaliation = document.querySelector('input[name="rate"]:checked');
 
 for (let index = 1; index <= 10; index += 1) {
   const label = document.createElement('label');
@@ -55,28 +47,3 @@ textarea.addEventListener('keyup', () => {
   const counterValue = 500 - textarea.value.length;
   counter.innerText = counterValue;
 });
-
-// function nome() {
-//   return `Nome: ${inputName.value} ${inputName2.value}`;
-// }
-
-// function showEmail() {
-//   return `Email: ${email.value}`;
-// }
-
-// function showHouse() {
-//   return `Casa: ${casa.value}`;
-// }
-
-// function showFamily() {
-//   return `Família: ${family.value}`;
-// }
-
-// function showMaterias() {
-//   return `Matérias: ${materias.value}`
-// }
-
-// submit.addEventListener('click', () => {
-//   clear.innerHTML = `<p>${nome()}</p><p>${showEmail()}</p>${showHouse()}
-//   <br>`;
-// });
