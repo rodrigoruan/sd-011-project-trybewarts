9fd2de6784060de7a8572d21bc788012aac9a47d
IsaacMagno <isaacmagno98@gmail.com>
2021-04-23 16:40:13 -0300

    desafio ate o 4

---
+++ script.js
@@ -0,0 +1,9 @@
+function alertFunction(){
+  const login = document.getElementById('login').value;
+  const senha = document.getElementById('senha').value;
+  if (login != 'tryber@teste.com' || senha != '123456'){
+    alert("Login ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+}
