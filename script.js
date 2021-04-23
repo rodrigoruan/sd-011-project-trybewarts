66da99369ceecbc518683bb2948f5e751582261c
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-23 18:29:13 -0300

    Correcoes linter

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const btnSubmit = document.querySelector('#btnSubmit'); // seletor botao logar
-const loginField = document.querySelector('#loginInfo'); // seletor input login
-const passwdField = document.querySelector('#passwdInfo'); // seletor input senha
-
 
 function submitLoginInfo(event) { // Funcao que valida o login, e exibe o alert adequado.
   event.preventDefault();
-  let loginValue = document.getElementById('loginInfo').value;
-  let passwdValue = document.getElementById('passwdInfo').value;
+  const loginValue = document.getElementById('loginInfo').value;
+  const passwdValue = document.getElementById('passwdInfo').value;
   if (loginValue !== 'tryber@teste.com' || passwdValue !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
