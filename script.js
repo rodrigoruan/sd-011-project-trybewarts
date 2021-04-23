bc0e54c176da1d402fb0f709af8188272c70b00e
Vitor HUgo <vitor.h.braz.leite@gmail.com>
2021-04-23 17:30:41 -0300

    Teste exerc3

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Exercício 3
 const loginButton = document.getElementById('button-logar');
-const valueLogin = document.getElementById('login').value;
-const valueSenha = document.getElementById('senha').value;
+const valueLogin = document.getElementById('login');
+const valueSenha = document.getElementById('senha');
 
 loginButton.addEventListener('click', () => {
-  if (valueLogin !== 'tryber@teste.com' || valueSenha !== '123456') {
-    alert('Login ou senha inválidos');
+  if (valueLogin.value !== 'tryber@teste.com' || valueSenha.value !== '123456') {
+    alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
