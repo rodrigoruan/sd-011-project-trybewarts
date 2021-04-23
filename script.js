8e92c0e32f281b9e4bd014d8e0177115f03c2fe8
Gabriel Carvalho <gabrielcarvalho466@gmail.com>
2021-04-23 17:29:59 -0300

    Acrecenta um formulário de autenticação no canto direito da barra superior

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('VQV');
+
+const submitLoginBtn = document.getElementById('header-submit');
+
+submitLoginBtn.addEventListener('click', verifiedValues)
+
+function verifiedValues() {
+  let loginValue = document.getElementById('login').value;
+  let passwordValue = document.getElementById('password').value;
+
+  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
+    alert('Olá, Tryber!')
+  } else {
+    alert('Login ou senha inválidos.')
+  }
+}
