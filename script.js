2a3afabba091f591905635c079e7bd67b3bdd10e
gab-boff <gmeninboff@gmail.com>
2021-04-23 18:06:52 -0300

    Arrumando CSS

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const loginButton = document.getElementById('login-button');
+const loginInput = document.getElementById('login');
+const passwordInput = document.getElementById('senha');
 
 loginButton.addEventListener('click', () => {
-  console.log('oi');
+  const loginValue = loginInput.value;
+  const passwordValue = passwordInput.value;
+
+  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 });
