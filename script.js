4e803dd6858bd1abb7bb43e65069c4d8d63f2bc1
Gisele Costa  <jikinha@gmail.com>
2021-04-23 17:21:36 -0300

    Arrumando o addEventListener

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const password = document.querySelector('#password');
 const btnLogin = document.querySelector('#login');
 
 btnLogin.addEventListener('click', () => {
-  if ((user !== 'tryber@teste.com') && (password === '123456')) {
+  if ((user === 'tryber@teste.com') && (password === '123456')) {
+    alert('Olá, Tryber!');
+  } else {
     alert('Login ou senha inválidos.');
-    return;
-  } alert('Olá, Tryber!');
+  }
 });
