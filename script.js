037bdf65f8e426cce06d75b8debddcf9a9ab7307
Tales Coelho <tales-coelho@live.com>
2021-04-23 15:42:34 -0300

    testando a parada do botão

---
+++ script.js
@@ -0,0 +1,10 @@
+const headerForm = document.querySelector('.header-form');
+console.log(headerForm);
+
+const loginSubmit = document.getElementById('trybewarts-login-submit');
+
+loginSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  alert('aeee');
+});
+
