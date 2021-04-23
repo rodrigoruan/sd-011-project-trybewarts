cbc06a21e643e36cd28828880b094e2dff39b536
Gisele Costa  <jikinha@gmail.com>
2021-04-23 17:44:57 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const password = document.querySelector('#password');
 const btnLogin = document.querySelector('#login');
 
 btnLogin.addEventListener('click', () => {
-  if ((user === 'tryber@teste.com') && (password === '123456')) {
-    alert('Olá, Tryber!');
-  } else {
+  if ((user !== 'tryber@teste.com') || (password !== '123456')) {
     alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
 });
