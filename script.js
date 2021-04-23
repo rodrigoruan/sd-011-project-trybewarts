fecf9eb15310ffe7b67305011af1ac3209efc068
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-23 17:53:16 -0300

    Correçoes lint

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function validateLoginForm() {
-  const user = document.forms['trybewarts-login']['user'];
-  const password = document.forms['trybewarts-login']['password'];
+const loginButton = document.querySelector('#login-button');
+
+loginButton.addEventListener('click', function() {
+  const user = document.forms.trybewarts-login.user;
+  const password = document.forms.trybewarts-login.password;
   if (user.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos');
   }
-}
+});
