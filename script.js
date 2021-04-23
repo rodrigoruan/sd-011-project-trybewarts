68729f0129e5eeec8d039882de8f3c0449a98a99
Gisele Costa  <jikinha@gmail.com>
2021-04-23 17:16:49 -0300

    Resolvendo linters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const password = document.querySelector('#password');
 const btnLogin = document.querySelector('#login');
 
 btnLogin.addEventListener('click', () => {
-  if (!(user === 'tryber@teste.com') && (password === '123456')) {
+  if ((user !== 'tryber@teste.com') && (password === '123456')) {
     alert('Login ou senha inválidos.');
     return;
   } alert('Olá, Tryber!');
