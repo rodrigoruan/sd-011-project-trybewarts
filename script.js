7675b36238bc10f0a707e9a53c32e80906d2cf2a
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-23 18:48:43 -0300

    ajustes no desafio 3

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const validateLogin = document.getElementById('loginButton');
 validateLogin.addEventListener('click', (event) => {
   event.preventDefault();
 
-  if (formValidate.elements[0].value === 'tryber@teste.com' && formValidate.elements[1].value === '123456') {
+  if (formValidate.elements[0].value === 'tryber@teste.com'
+  && formValidate.elements[1].value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
