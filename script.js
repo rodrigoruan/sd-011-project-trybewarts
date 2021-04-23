b5bdd8d4f6691a4135170049c21ac235ed1b5fef
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-23 18:06:51 -0300

    Adiciona mudanças

--- script.js
+++ script.js
@@ -2,14 +2,15 @@ const formLogin = document.querySelector('.trybewarts-login');
 const inputEmail = document.querySelector('#login');
 const inputSenha = document.querySelector('#senha');
 
-function verificaLogin() {
+function verificaLogin(event) {
+  event.preventDefault();
   const email = inputEmail.value;
   const senha = inputSenha.value;
   const loginPadrao = 'tryber@teste.com';
   const senhaPadrao = '123456';
 
   if (email !== loginPadrao || senha !== senhaPadrao) {
-    alert('Login ou senha inválidos.');
+    alert('Login ou senha inválidos');
   } else {
     alert('Olá, Tryber!');
   }
