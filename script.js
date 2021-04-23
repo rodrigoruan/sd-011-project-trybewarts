6f7823711b04dc1545a5e875674a26451254b6a5
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-23 12:37:45 -0700

    Requisito 3

---
+++ script.js
@@ -0,0 +1,13 @@
+function handleLogin() {
+  const loginText = document.getElementById("login").value;
+  const passwordText = document.getElementById("senha").value;
+  // fazer um if que verifica se o login é tryber@test.com e senha 123456
+  if ((loginText === "tryber@teste.com") & (passwordText === "123456")) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Login ou senha inválidos");
+  }
+}
+
+const button = document.getElementById("botao");
+button.addEventListener("click", handleLogin);
