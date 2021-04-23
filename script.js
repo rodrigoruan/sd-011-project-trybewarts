6b3559d21a371d6ba6e4ff94f5dbeb46cad1ad35
Luan Xavier <luantenorio3@gmail.com>
2021-04-23 18:01:48 -0300

    Requisito 3 finalizado

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+const button = document.querySelector('.entrar');
 
+button.addEventListener('click', () => {
+  if (login.value !== 'tryber@teste.com' || senha.value !== '123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+});
