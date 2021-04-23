e41d0e94c2bc7100a4a6878e25be08f8f3117c7c
MIkaela Braga <bragamikaela@gmail.com>
2021-04-23 18:07:32 -0300

    requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+function alerta() {
+  const getButton = document.getElementById('button1');
+  const getEmail = document.getElementById('email');
+  const getSenha = document.getElementById('senha');
+
+  getButton.addEventListener('click', () => {
+    if (getEmail.value === 'tryber@teste.com' && getSenha.value === '123456') {
+      alert('Olá, Tryber!');
+    } else {
+      alert('Login ou senha inválidos');
+    }
+  });
+}
+alerta();
