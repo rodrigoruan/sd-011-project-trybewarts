6e829fc7fc9ed50d789fb14c7604f65eb1df053f
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-23 12:47:21 -0700

    Correções

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function handleLogin() {
-  const loginText = document.getElementById("login").value;
-  const passwordText = document.getElementById("senha").value;
+  const loginText = document.getElementById('login').value;
+  const passwordText = document.getElementById('senha').value;
   // fazer um if que verifica se o login é tryber@test.com e senha 123456
-  if ((loginText === "tryber@teste.com") & (passwordText === "123456")) {
-    alert("Olá, Tryber!");
+  if ((loginText === 'tryber@teste.com') && (passwordText === '123456')) {
+    alert('Olá, Tryber!');
   } else {
-    alert("Login ou senha inválidos");
+    alert('Login ou senha inválidos');
   }
 }
 
-const button = document.getElementById("botao");
-button.addEventListener("click", handleLogin);
+const button = document.getElementById('botao');
+button.addEventListener('click', handleLogin);
