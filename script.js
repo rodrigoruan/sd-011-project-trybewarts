f91600c7f6643295ac8196d4fee68d27e77e4402
Hugo Mouto <hmoutol@gmail.com>
2021-04-23 15:18:29 -0300

    Iniciando Projeto - Espancando em Dupla

---
+++ script.js
@@ -0,0 +1,16 @@
+// // get login input
+// let loginInput = document.getElementById('login');
+// // get password input
+// let passwordInput = document.getElementById('password');
+
+function validateTryber () {
+  // get login input
+  let loginInput = document.getElementById('login');
+  // get password input
+  let passwordInput = document.getElementById('password');
+  if (loginInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
+    alert('Login ou senha inválidos.'); 
+  } else {
+    alert('Olá, Tryber!')
+  }
+}
