40d02dde69d09bcaa10d38a18c03bafab5465adc
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-04-23 16:34:54 -0300

    req 1,2,4

---
+++ script.js
@@ -0,0 +1,9 @@
+// req 3 alerta ao logar
+ /* function Login(){
+ if (document.getElementById("email") === "tryber@teste.com" && document.getElementById("senha") === "123456"){
+  alert('Login ou senha inválidos.')
+ }else {
+     alert('Olá, Tryber!')
+ }
+}
+addEventListener.Login(); */
