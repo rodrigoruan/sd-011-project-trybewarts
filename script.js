d1389b7dd8df2896534589008a7a4d895b8fff64
jeferson casagrande <jefersoncasagrandedesouza@gmail.com>
2021-04-23 18:49:16 -0300

    requisito 13

--- script.js
+++ script.js
@@ -2,15 +2,20 @@
 const validEmail = 'tryber@teste.com';
 const validPassword = '123456';
 const houses = ['Gitnória', 'Reactpuff', 'Corvinode', 'Pytherina'];
+const inputs = ['HoFs', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
+const labels = ['Higher Order Functions', 'Jest', 'Promises', 'React', 'SQL', 'Python'];
 
 /* Variaveis do DOM */
 const inputLogin = document.querySelector('.trybewarts-login input[type="email"]');
 const inputPassword = document.querySelector('.trybewarts-login input[type="password"]');
 const buttonSubmitLogin = document.getElementById('submitLogin');
 const selectHouse = document.getElementById('house');
+const labelContent = document.getElementById('label-content');
 
 /* DOM Elements */
 const optionElement = document.createElement('option');
+const inputElement = document.createElement('input');
+const labelElement = document.createElement('label');
 
 function validateLogin(event) {
   if (inputLogin.value === validEmail && inputPassword.value === validPassword) {
@@ -32,9 +37,22 @@ function createSelectHouseOptions() {
   });
 }
 
+function createCheckbox() {
+  inputs.reverse().forEach((input, index) => {
+    const newCheckbox = inputElement.cloneNode();
+    newCheckbox.value = input;
+    newCheckbox.setAttribute('type', 'checkbox');
+    const newLabel = labelElement.cloneNode();
+    newLabel.appendChild(newCheckbox);
+    newLabel.appendChild(document.createTextNode(labels[index]));
+    labelContent.insertAdjacentElement('afterend', newLabel);
+  });
+}
+
 function onLoad() {
   buttonSubmitLogin.addEventListener('click', validateLogin);
   createSelectHouseOptions();
+  createCheckbox();
 }
 
 window.onload = onLoad;
