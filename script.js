440d49ed6dfc0990d1aeb7f6c8ad8c96e4603696
LuanAlexandre <luanrb19@gmail.com>
2021-04-23 15:51:40 -0400

    trybewarts project - requisito 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const emailLogin = document.getElementById('email-login').value;
+const password = document.getElementById('password-login').value;
+
+function verificaDados() {
+  const btnLogin = document.getElementById('btn-login');
+  btnLogin.addEventListener('click', () => {
+    ((emailLogin === 'tryber@teste.com') && (password === 123456)) ? alert('Olá, Tryber!') : alert('Login ou senha inválidos.');
+  });
+}
+
+verificaDados();
