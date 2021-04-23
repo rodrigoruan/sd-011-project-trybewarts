b5b4e86d6590892ae1abd4f70446812577c3c62c
Daniel <danielfdoficial@gmail.com>
2021-04-23 16:39:48 -0300

    conclui desafio 3 e talvez o 2

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('botao');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
+
+function validateLogin() {
+  if (login.value + password.value !== 'tryber@teste.com123456') {
+    alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
+  }
+} 
+button.addEventListener('click', validateLogin);
