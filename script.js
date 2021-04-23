46d24b530339e5303d07bfac285427e58e123c25
LuanAlexandre <luanrb19@gmail.com>
2021-04-23 15:59:23 -0400

    trybewarts project - requisito 14 - reparacoes do lint

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const password = document.getElementById('password-login').value;
 function verificaDados() {
   const btnLogin = document.getElementById('btn-login');
   btnLogin.addEventListener('click', () => {
-    ((emailLogin === 'tryber@teste.com') && (password === 123456)) ? alert('Olá, Tryber!') : alert('Login ou senha inválidos.');
+    ((emailLogin === 'tryber@teste.com') && (password === 123456)) ? 
+    alert('Olá, Tryber!') : alert('Login ou senha inválidos.');
   });
 }
 
