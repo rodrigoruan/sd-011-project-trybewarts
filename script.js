5916b17fd7f483d0092e42ba73d1aac72adc5c0a
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-23 20:37:06 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnLogin = document.getElementById('btnLogar');
 const inputLogin = document.getElementById('login');
 const inptPassword = document.getElementById('password');
+const btnCheck = document.getElementById('agreement');
+const btnSub = document.getElementById('submit-btn');
 
 function LoginAlert() {
   if (inputLogin.value === 'tryber@teste.com' && inptPassword.value === '123456') {
@@ -11,3 +13,13 @@ function LoginAlert() {
 }
 
 btnLogin.addEventListener('click', LoginAlert);
+
+btnSub.disabled = true;
+
+function validateButton() {
+  if (btnCheck.checked === true) {
+    btnSub.disabled = false;
+  }
+}
+
+btnCheck.addEventListener('click', validateButton);
