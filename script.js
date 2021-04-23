a44ed35248807952f66968c39d58c45d479bbcc6
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-04-23 18:59:49 -0300

    Item 1-10 erro em 3 e 5

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateTryber () {
   let loginInput = document.getElementById('login');
   // get password input
   let passwordInput = document.getElementById('password');
-  if (loginInput.value !== 'tryber@teste.com' && passwordInput.value !== '123456') {
+  if (loginInput.value !== 'tryber@teste.com' || passwordInput.value !== '123456') {
     alert('Login ou senha inválidos.'); 
   } else {
     alert('Olá, Tryber!')
