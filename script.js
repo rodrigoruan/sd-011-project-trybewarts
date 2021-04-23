91ea6bbdfd658851facc3b11806655cde5b49f5c
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-23 18:09:41 -0300

    Req 3 - JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const loginButton = document.getElementById('login-button');
+const loginInput = document.getElementById('login-input');
+const loginPassword = document.getElementById('password-input');
+
+
+loginButton.addEventListener('click', () => {
+  // event.preventDefault();
+  if (loginInput.value === 'tryber@teste.com' && loginPassword.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+});
