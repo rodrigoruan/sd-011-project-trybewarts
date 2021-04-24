11cbf538ed2ddad40aa587c80e17952ff12fd3af
Michel Pereira <michelbecauro@gmail.com>
2021-04-24 02:19:48 -0300

    Requisito 3: EsLint e StyleLint correções

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-
-function start () {
-	
-	function validaLogin () {
-
-		var inputEmail = document.querySelector('#email-login');
-		var inputSenha = document.querySelector('#password-login');
-			
-		// let formLogin = document.getElementsByClassName('trybewarts-login')[0]; // Também Funciona
-			
-		let btnLogar = document.getElementById('btnLogar');
-
-		btnLogar.addEventListener('click', function () {
-			
-		// formLogin.addEventListener('submit', function () { // Também funciona
-
-			if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
-					alert ('Olá, Tryber!');
-			} else {
-				alert ('Login ou senha inválidos.');
-			}
-		});
-	}
-
-validaLogin ();
+function start() {
+  function validaLogin() {
+    const inputEmail = document.querySelector('#email-login');
+    const inputSenha = document.querySelector('#password-login');
+    // let formLogin = document.getElementsByClassName('trybewarts-login')[0]; // Também Funciona
+    const btnLogar = document.getElementById('btnLogar');
+    btnLogar.addEventListener('click', () => { // formLogin.addEventListener('submit', function () { // Também funciona
+      if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+        alert('Olá, Tryber!');
+      } else {
+        alert('Login ou senha inválidos.');
+      }
+    });
+  }
+  validaLogin();
 }
-
 window.onload = start;
