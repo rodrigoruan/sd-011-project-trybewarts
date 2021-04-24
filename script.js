77f4fa26a15b270a0cbc0cbb9aa582cb190db2a1
Aline Hofner <alinehofner@gmail.com>
2021-04-24 16:12:15 -0300

    Remove requisito 20

--- script.js
+++ script.js
@@ -23,20 +23,3 @@ const submitButton = document.querySelector('#submit-btn');
 agreeCheck.addEventListener('click', () => {
   submitButton.removeAttribute('disabled');
 });
-
-const textArea = document.getElementById('textarea');
-function counterText() {
-  const countInit = 500;
-  const maxLength = document.getAttribute('maxlength');
-  const currentLength = textArea.length;
-  const minLength = document.getAttribute('minLength');
-  if (currentLength === minLength) {
-    document.getElementById('counter').innerText = '500';
-  } else if (currentLength > minLength && currentLength <= maxLength) {
-    const countAtual = countInit - currentLength;
-    document.getElementById('counter').innerText = countAtual;
-  }
-}
-window.onload = () => {
-  textArea.addEventListener('input', counterText);
-};
