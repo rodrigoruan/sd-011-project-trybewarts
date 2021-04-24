6d6b409b214158044a73c3bea0ca22b187ba0859
Gustavo Miyagawa - Turma 11 <miyagawasantos@hotmail.com>
2021-04-24 19:44:19 -0400

    Adicionado script para aceitar login e senha conforme requisito 3.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const loginBtn = document.querySelectorAll('.Submit-btn')[0];
+const loginText = document.querySelectorAll('input')[0];
+const passwordText = document.querySelectorAll('input')[1];
+
+function alertLogin() {
+  if (loginText.value === 'tryber@teste.com' && passwordText.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+}
+
+loginBtn.addEventListener('click', alertLogin);
+
 document.getElementById('submit-btn').disabled = true;
