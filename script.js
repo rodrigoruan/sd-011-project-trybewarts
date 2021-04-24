67bfccc2d4e8c4ac9e5666fa5cab7a4c14547b6e
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-24 09:20:59 -0300

    pull atualizações

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function loginAlert() {
   const loginButton = document.getElementById('login-button');
   const loginEmail = document.getElementById('email-form').value;
   const loginPassword = document.getElementById('password-form').value;
-  loginButton.addEventListener('click', () => {
+  /* loginButton.addEventListener('click', () => { */
     if (loginEmail === 'tryber@teste.com' && loginPassword === '123456') {
       alert('Olá, Tryber!');
     } else {
       alert('Login ou senha inválidos.');
     }
-  });
+  /* }); */
 }
 loginAlert();
