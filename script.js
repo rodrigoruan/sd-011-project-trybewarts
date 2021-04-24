ab404a1f9ccbd9c1e7ed566ca470ae18cf0ca1c4
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-24 02:41:28 -0300

    correcao lint e ativa enviar apenas com checked marcado

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const loginButton = document.getElementById('login-button');
 // Dentro da funço que é executada ao clicar no botão, deve-se verificar se o email.
 // que esta no input de email é igual a tryber@teste.com e senha igual a 123456.
 loginButton.addEventListener('click', () => {
-  if (document.getElementById('login').value === 'tryber@teste.com' && document.getElementById('senha').value === '123456') {
+  if (document.getElementById('login').value === 'tryber@teste.com' &&
+    document.getElementById('senha').value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
-    });
+});
+
+//Criar constante que recebe o botao Submit
+const agreementCheck = document.getElementById('agreement');
+agreementCheck.addEventListener('click', () => {
+  if (document.getElementById('agreement').checked === true) {
+    document.getElementById('submit-btn').disabled = false;
+  } else {
+    document.getElementById('submit-btn').disabled = true;
+  }
+})
