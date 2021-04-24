18ab7c86f8cdaaa3f5c2ffb625d6cdd64d84fa16
Thyago Pessoa <flash_thyago@hotmail.com>
2021-04-24 19:52:10 -0300

    Desafio 20.2

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const loginField = document.querySelector('#login-field');
 const passwordField = document.querySelector('#password-field');
 const loginButton = document.querySelector('#login-button');
-const getForm = document.querySelector('#evaluation-form');
 const getBtn = document.querySelector('#submit-btn');
 const getCheckBox = document.querySelector('#agreement');
 const getCounter = document.querySelector('#counter');
 const getTextArea = document.querySelector('#textarea');
-const firstName = document.querySelector('#input-name');
-const lastName = document.querySelector('#input-lastname');
-const getEmail = document.querySelector('#input-email');
-const getHouse = document.querySelector('#house');
-const getObs = document.querySelector('#textarea');
 
 function alertLogin() {
   if (loginField.value === 'tryber@teste.com' && passwordField.value === '123456') {
