e78aa6e44cce15f5761b3e65446496aecba68b52
gab-boff <gmeninboff@gmail.com>
2021-04-24 09:56:40 -0300

    Adicionando Desafio 7

--- script.js
+++ script.js
@@ -6,16 +6,9 @@ loginButton.addEventListener('click', () => {
   const loginValue = loginInput.value;
   const passwordValue = passwordInput.value;
 
-<<<<<<< HEAD
-  if (loginValue === 'tryber@teste.com' && passwordValue === '123456') {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Login ou senha inválidos.');
-=======
   if (loginValue !== 'tryber@teste.com' && passwordValue !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
->>>>>>> 30a311ac8b6b120af735b701afad4c53e8328743
   }
 });
