b6d8f6984fe1607fbbde1338092cb23bf79f2971
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-24 01:16:38 -0300

    Corrigindo requisito 20

--- script.js
+++ script.js
@@ -47,12 +47,15 @@ function enableSubmit() {
   });
 }
 
+// contar caracteres
 function countCharacters() {
   const counter = document.getElementById('counter');
-  const textarea = document.getElementById('comment');
+  const textarea = document.getElementById('textarea');
   let count = 500;
-
+  
+  // Usei o 'keyup' pra poder pegar o valor a cada vez que for clicar numa tecla
   textarea.addEventListener('keyup', () => {
+    // Aqui eu diminuí 500 que é o valor original pelo tamanho do que foi escrito na textarea
     count = 500 - textarea.value.length;
     counter.innerText = count;
   });
