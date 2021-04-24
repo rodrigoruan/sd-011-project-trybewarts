7e7cdb651a11842a310b84d8bd15943c91afe27c
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-24 16:40:41 -0300

    Requisitos 12, 13 e 14 Realizados

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btnSubmit = document.querySelector('#btnSubmit'); // seletor botao logar
+const evaluateFieldDiv = document.querySelector('.evaluateField'); // seletor camp de avaliacao
 
 function submitLoginInfo(event) { // Funcao que valida o login, e exibe o alert adequado.
   event.preventDefault();
@@ -12,3 +13,23 @@ function submitLoginInfo(event) { // Funcao que valida o login, e exibe o alert 
 }
 
 btnSubmit.addEventListener('click', submitLoginInfo);
+
+function createEvaluateRadios() {
+  let n = 1;
+  while (n < 11) {
+    const radioToCreateLabel = document.createElement('label');
+    const radioToCreateTag = document.createElement('input');
+    radioToCreateLabel.innerText = n.toString();
+    radioToCreateLabel.setAttribute('for', n.toString());
+    radioToCreateTag.setAttribute('type', 'radio');
+    radioToCreateTag.setAttribute('name', 'rate');
+    radioToCreateTag.setAttribute('value', n.toString());
+    evaluateFieldDiv.appendChild(radioToCreateTag);
+    evaluateFieldDiv.appendChild(radioToCreateLabel);
+    n += 1;
+  }
+}
+
+createEvaluateRadios();
+
+console.log(evaluateFieldDiv);
