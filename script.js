724f3b33d536663168173d7e6c8845fdd6695627
IgorMendez <igor.emp@hotmail.com>
2021-04-24 07:04:26 -0300

    Ajustes

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const buttonLogin = document.getElementById('button-login');
 const loginInput = document.getElementById('login');
 const passwordInput = document.getElementById('password');
 // Functions
+
 function login() {
-  if (loginInput.value === 'tryber@teste.com' || passwordInput.value === '123456') {
+  if (loginInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 }
 
-window.onload = function load() {
-  buttonLogin.addEventListener('click', login);
-};
+buttonLogin.addEventListener('click', login);
