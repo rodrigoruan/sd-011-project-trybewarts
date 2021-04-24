3da36817ca115ac3e164a5e6648566769cef6c5b
Ana Carolina R Santos <kroline0693@gmail.com>
2021-04-24 18:02:45 -0300

    consertado requisitos 1 ao 3

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Botão Login
 const email = document.getElementById('login');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('button');
 
 botao.addEventListener('click', () => {
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+  if (email.value === 'tryber@teste.com' || senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 });
+
+// Botão formulário;
