e625315f0350c49b7b5625e3dc6c5acb82e556f1
IgorMendez <igor.emp@hotmail.com>
2021-04-24 07:19:37 -0300

    Exercício 8

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const buttonLogin = document.getElementById('button-login');
 const loginInput = document.getElementById('login');
 const passwordInput = document.getElementById('password');
-// Functions
+// Function
 
 function login() {
   if (loginInput.value === 'tryber@teste.com' && passwordInput.value === '123456') {
