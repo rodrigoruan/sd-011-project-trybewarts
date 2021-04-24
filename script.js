8aa63db4cb671fb768119931b65890bf1434a043
Felipe Buso <felipe_buso@hotmail.com>
2021-04-24 18:03:42 -0300

    item 21

--- script.js
+++ script.js
@@ -42,3 +42,50 @@ txtArea.addEventListener('keyup', () => {
   const nowLength = txtArea.value.length;
   txtCounter.innerText = 500 - nowLength;
 });
+
+const nome = document.getElementById('input-name');
+const sobrenome = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const casa = document.getElementById('house');
+const familia = document.getElementsByClassName('familia');
+const materias = document.getElementsByClassName('subject');
+const avaliacao = document.getElementsByClassName('nota');
+const obs = document.getElementById('textarea');
+const nomeLabel = 'Nome: ';
+const emailLabel = 'Email: ';
+const casaLabel = 'Casa: ';
+const familiaLabel = 'Família: ';
+const materiasLabel = 'Matérias: ';
+const avaliacaoLabel = 'Avaliação: ';
+const obsLabel = 'Observações: ';
+const space = ' ';
+
+function recoveryRadio(values) {
+  const txtValue = [];
+  for (let index = 0; index < values.length; index += 1) {
+    if (values[index].checked === true) {
+      txtValue.push(values[index].value);
+    }
+  }
+  return txtValue.join(', ');
+}
+
+sendButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const selectForm = document.getElementById('evaluation-form');
+  const selectChild = document.getElementById('all-form');
+  const extFamilia = recoveryRadio(familia);
+  const extMaterias = recoveryRadio(materias);
+  const extAvaliacao = recoveryRadio(avaliacao);
+  const newForm = [nomeLabel + nome.value + space + sobrenome.value, emailLabel + email.value,
+    casaLabel + casa.value, familiaLabel + extFamilia, materiasLabel + extMaterias,
+    avaliacaoLabel + extAvaliacao, obsLabel + obs.value];
+  selectForm.removeChild(selectChild);
+  const newUl = document.createElement('ul');
+  selectForm.appendChild(newUl);
+  for (let index = 0; index < newForm.length; index += 1) {
+    const newLine = document.createElement('li');
+    newLine.innerText = newForm[index];
+    newUl.appendChild(newLine);
+  }
+});
