2fb3654df6a359f0d022cb8793bff58e8121814a
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-24 02:46:20 -0300

    lint

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const loginButton = document.getElementById('login-button');
 // Dentro da funço que é executada ao clicar no botão, deve-se verificar se o email.
 // que esta no input de email é igual a tryber@teste.com e senha igual a 123456.
 loginButton.addEventListener('click', () => {
-  if (document.getElementById('login').value === 'tryber@teste.com' 
+  if (document.getElementById('login').value === 'tryber@teste.com'
     && document.getElementById('senha').value === '123456') {
     alert('Olá, Tryber!');
   } else {
