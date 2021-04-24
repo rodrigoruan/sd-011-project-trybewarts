d3569846a6f8f8bad3e3c3d04671a20626d8a9e2
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-24 11:28:31 -0300

    testa requisito 3 -evaluator

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Criar um addEventListener no botão LOGAR.
 const loginButton = document.getElementById('login-button');
 // Dentro da funço que é executada ao clicar no botão, deve-se verificar se o email.
-// que esta no input de email é igual a tryber@teste.com e senha igual a 123456.
+// O email que está no input de email é igual a tryber@teste.com e input de senha igual a 123456.
 loginButton.addEventListener('click', () => {
-  if (document.getElementById('login').value === 'tryber@teste.com'
-    || document.getElementById('senha').value === '123456') {
-    alert('Olá, Tryber!');
-  } else {
+  if (document.getElementById('login').value !== 'tryber@teste.com'
+    && document.getElementById('senha').value !== '123456') {
     alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
 });
 
