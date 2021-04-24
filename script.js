48dd99eafa69cf48cd291cc10f1f724d0a3829ce
Ghislaine <glenox79@gmail.com>
2021-04-24 11:26:10 -0300

    exercico 03 validacao

---
+++ script.js
@@ -0,0 +1,14 @@
+const btnLogin = document.querySelector('#btn-login');
+function validateLoginPassword() {
+  const form1 = document.querySelector('.trybewarts-login');
+  const login = document.querySelector('#inputEmail1');
+  const password = document.querySelector('#inputPassword1');
+  if (login.value !== 'tryber@teste.com' || password.value !== '123456') {
+    alert('Login ou senha inválidos');
+    login.focus();
+  } else {
+    alert('Olá, Tryber!');
+    form1.value = '';
+  }
+}
+btnLogin.addEventListener('click', validateLoginPassword);
