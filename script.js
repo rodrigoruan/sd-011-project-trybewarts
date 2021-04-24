caea4c723bec11e59e47ae5d0a4faa5d80cf4c6a
PH Assis <pieit@live.com>
2021-04-24 02:50:52 -0300

    clgh 21 ready

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const password = document.getElementById('password-input');
 const button = document.getElementById('login-button');
 const submitButton = document.getElementById('submit-btn');
 const agreement = document.getElementById('agreement');
+const counter = document.getElementById('counter');
+const textareaInput = document.getElementById('textarea');
+const evaluationForm = document.getElementById('evaluation-form');
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const houseType = document.getElementById('house');
+
 function loginTry() {
   if (login.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
@@ -20,3 +28,43 @@ agreement.addEventListener('input', () => {
     submitButton.setAttribute('disabled', 'true');
   }
 });
+
+textareaInput.addEventListener('input', () => {
+  if (textareaInput.value.length === 0) {
+    counter.innerText = '500';
+  } else {
+    counter.innerText = 500 - textareaInput.value.length;
+  }
+});
+
+function materiasChecked() {
+  const x = document.querySelectorAll('.subject:checked');
+  const y = [];
+  for (let i = 0; i < x.length; i += 1) {
+    y.push(` ${x[i].value}`);
+  }
+  return y;
+}
+
+function allForms(event) {
+  const allFields = {
+    Nome: `${inputName.value} ${inputLastName.value}`,
+    Email: `${inputEmail.value}`,
+    Casa: `${houseType.value}`,
+    Família: `${document.querySelector('input[name="family"]:checked').value}`,
+    Matérias: materiasChecked(),
+    Avaliação: ` ${document.querySelector('input[name="rate"]:checked').value}`,
+    Observações: `${textareaInput.value}`,
+  };
+  let y = '';
+  for (const x of Object.keys(allFields)) {
+    y += `${x}: ${allFields[x]}<br>`;
+
+    event.preventDefault();
+  }
+  console.log(y);
+  evaluationForm.innerHTML = y;
+  event.preventDefault();
+}
+
+submitButton.addEventListener('click', allForms);
