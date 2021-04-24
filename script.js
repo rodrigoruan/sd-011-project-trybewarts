dff7e06802759747e91f13f3de4226a3bdce2bd5
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-24 10:55:53 -0300

    corrige erros do lint e refatora funçao para alinhamento com lint

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-window.onload = function () {
-  const submit = document.getElementById('submit-button');
+const submit = document.getElementById('submit-button');
+const login = document.getElementById('login');
+const password = document.getElementById('password');
 
-  function submitForm() {
-    const login = document.getElementById('login');
-    const password = document.getElementById('password');
-    const standardLogin = 'tryber@teste.com';
-    const standardPassword = '123456';
-    if ((login.value === standardLogin) && (password.value === standardPassword)) {
-      alert('Olá, Tryber!');
-    } else {
-      alert('Login ou senha inválidos.');
-    }
+function submitForm() {
+  const standardLogin = 'tryber@teste.com';
+  const standardPassword = '123456';
+  if (login.value === standardLogin && password.value === standardPassword) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
   }
+}
 
-  submit.addEventListener('click', submitForm);
-};
+submit.addEventListener('click', submitForm);
