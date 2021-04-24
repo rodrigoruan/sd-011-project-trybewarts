5cf5d539d390bd9e20f0ee9153d15a1acd842480
marsleite <marsleite@gmail.com>
2021-04-24 03:09:48 -0300

    Desafio 20 bônus - Project TrybeWarts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const email = document.querySelector('#email');
 const senha = document.querySelector('#senha');
 const submit = document.querySelector('#button');
+const textArea = document.querySelector('#textarea');
 
 submit.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -20,3 +21,17 @@ check.addEventListener('click', () => {
     enviar.setAttribute('disabled', 'disabled');
   }
 });
+
+function limits() {
+  let textValue = document.querySelector('#textarea').value;
+  const limit = 500;
+  const total = textValue.length;
+  if (total <= limit) {
+    const rest = limit - total;
+    document.getElementById('counter').innerHTML = rest;
+  } else {
+    textValue = textValue.substr(0, limit);
+  }
+}
+
+textArea.addEventListener('keyup', limits);
