148703a1b65dae9889232eb69a1403543c5a47ee
Reginara Gomes <refagomes@gmail.com>
2021-04-24 01:03:50 -0300

    Adiciona requisito 4.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const loginButton = document.querySelector('#button-login');
 
 loginButton.addEventListener('click', () => {
-  const trybewartsLogin = document.forms('trybewarts-login');
+  const trybewartsLogin = document.forms['trybewarts-login'];
   const user = trybewartsLogin.user.value;
   const password = trybewartsLogin.password.value;
   if (user === 'tryber@teste.com' && password === '123456') {
