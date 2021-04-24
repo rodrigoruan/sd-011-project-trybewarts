b3c0456a1838064a04e50443623c53901d785ac4
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-24 01:38:30 -0300

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // function validateLogin() {
 //   const loginButton = document.getElementById('login-button');
-//   loginButton.addEventListener('click', clickLogin);
+//   loginButton.addEventListener('submit', clickLogin);
 // }
 
 // window.onload = function load() {
