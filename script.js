8748346b7a92bfef5961f59cec625399918a6bf8
Hamander <1hamander@gmail.com>
2021-04-24 09:55:56 -0300

    Lint Fixes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const submitButton = document.querySelector('#submit-button');
 function validation() {
   const login = document.querySelector('#login').value;
   const pass = document.querySelector('#pass').value;
-  if (login != 'tryber@teste.com' && pass != '123456') {
+  if (login !== 'tryber@teste.com' && pass !== '123456') {
     alert('Login ou senha inválidos');
   } else {
     alert('Olá Tryber!');
