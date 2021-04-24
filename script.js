cb922d39837c453e360c12f3f261804e810898b4
Dario Junior <dario.junior.rs@gmail.com>
2021-04-23 21:19:50 -0300

    falta ultimo req.4 e 21

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
+const checkBox = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
+const textArea = document.querySelector('#text-coment');
+const counter = document.querySelector('#counter');
+const mainForm = document.querySelector('#evaluation-form');
+const formInformations = document.querySelector('#informacoes');
+
 function clickLogin() {
   const email = event.path[2][0].value;
   const password = event.path[2][1].value;
 
   email === 'tryber@teste.com' && password === '123456'
     ? window.alert('Olá, Tryber!')
-    : window.alert('email ou senha inválida');
+    : window.alert('Login ou senha inválidos.');
 };
 
-// function validateLogin() { 
-//   const loginButton = document.getElementById('login-button');
-//   loginButton.addEventListener('click', clickLogin);
-// }
+checkBox.addEventListener("change", () => {
+  checkBox.checked ? btnSubmit.disabled = false
+    : btnSubmit.disabled = true;
+})
 
-// window.onload = function load() {
 
-// };
+textArea.addEventListener('keyup', () => {
+  counter.innerText = textArea.value.length.toString();
+})
+
+
+btnSubmit.addEventListener('click', (event) => {
+  mainForm.style.display = 'none';
+  event.preventDefault();
+  const target = event.path[3];
+  for (let index = 1; index < event.path.length; index += 1) {
+    const inputText = document.createElement('input');
+    formInformations.appendChild(inputText);
+    inputText.value = target[index].value;
+  }
+  formInformations.style.display = 'block';
+})
