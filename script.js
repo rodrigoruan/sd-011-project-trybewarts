80a6fbd27d0eb2cff93cfeaef7cfb8b1f73caf70
Reginara Gomes <refagomes@gmail.com>
2021-04-24 00:40:49 -0300

    Adiciona requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginButton = document.querySelector('#button-login');
+
+loginButton.addEventListener('click', () => {
+    const trybewartsLogin = document.forms('trybewarts-login');
+    const user = trybewartsLogin.user.value;
+    const password = trybewartsLogin.password.value;
+    if (user === 'tryber@teste.com' && password === '123456') {
+        alert('Olá, Tryber!');
+     } else {
+         alert('Login ou senha inválidos.');
+        }
+});
