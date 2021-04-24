54c7e439ca1bbfdf3b7e3b8cbef49f05da34da76
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-24 14:27:18 -0300

    Adiciona o requisito 21

--- script.js
+++ script.js
@@ -40,3 +40,124 @@ textArea.addEventListener('input', () => {
     contChar.innerText = 500 - textArea.value.length;
   }
 });
+
+//
+
+function removeForm() {
+  const evaluationFrom = document.querySelector('#evaluation-form');
+  const main = document.querySelector('main');
+
+  main.removeChild(evaluationFrom);
+}
+
+function nameLastName() {
+  const createParagraph = document.createElement('p');
+
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+
+  createParagraph.innerText = `Nome: ${name} ${lastName}`;
+
+  return createParagraph;
+}
+
+function returnEmail() {
+  const createParagraph = document.createElement('p');
+
+  const returnEmailValue = document.querySelector('#input-email').value;
+
+  createParagraph.innerText = `Email: ${returnEmailValue}`;
+
+  return createParagraph;
+}
+
+function returnHouse() {
+  const createParagraph = document.createElement('p');
+
+  const returnHouseValue = document.querySelector('#house').value;
+
+  createParagraph.innerText = `Casa: ${returnHouseValue}`;
+
+  return createParagraph;
+}
+
+function returnFamily() {
+  const family = document.getElementsByName('family');
+  let selectedFamily;
+
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked === true) {
+      selectedFamily = family[index].value;
+    }
+  }
+
+  const createParagraph = document.createElement('p');
+
+  createParagraph.innerText = `Família: ${selectedFamily}`;
+
+  return createParagraph;
+}
+
+function returnSubjects() {
+  const subjects = document.getElementsByClassName('subject');
+  let selectedSubjects = [];
+
+  for (let index = 0; index < subjects.length; index += 1) {
+    if (subjects[index].checked === true) {
+      selectedSubjects.push(` ${subjects[index].value}`);
+    }
+  }
+
+  selectedSubjects = selectedSubjects.toString();
+
+  const createParagraph = document.createElement('p');
+
+  createParagraph.innerText = `Matérias: ${selectedSubjects}`;
+
+  return createParagraph;
+}
+
+function avaliation() {
+  const rate = document.getElementsByName('rate');
+  let selectedRate;
+
+  for (let index = 0; index < rate.length; index += 1) {
+    if (rate[index].checked === true) {
+      selectedRate = rate[index].value;
+    }
+  }
+
+  const createParagraph = document.createElement('p');
+
+  createParagraph.innerText = `Avaliação: ${selectedRate}`;
+
+  return createParagraph;
+}
+
+function comment() {
+  const createParagraph = document.createElement('p');
+
+  const returnComment = document.querySelector('#textarea').value;
+
+  createParagraph.innerText = `Observações: ${returnComment}`;
+
+  return createParagraph;
+}
+
+function createFormElement() {
+  const createForm = document.createElement('form');
+  createForm.id = 'evaluation-form';
+
+  createForm.appendChild(nameLastName());
+  createForm.appendChild(returnEmail());
+  createForm.appendChild(returnHouse());
+  createForm.appendChild(returnFamily());
+  createForm.appendChild(returnSubjects());
+  createForm.appendChild(avaliation());
+  createForm.appendChild(comment());
+  document.querySelector('main').appendChild(createForm);
+
+  removeForm();
+}
+
+document.querySelector('#submit-btn').addEventListener('click', createFormElement);
