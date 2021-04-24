ea974119a9fa7a53e3d3e5b2134452dfe623bb5f
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-24 09:55:29 -0300

    Corrige exercício 3

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-/* const loginButton = document.getElementById('login-button');
-loginButton.addEventListener('click', () => {
-  console.log('oi');
-});
- */
-/* const loginButton = document.getElementById('login-button'); */
-
 function loginAlert() {
-  const loginButton = document.getElementById('login-button');
-  const loginEmail = document.getElementById('email-form').value;
-  const loginPassword = document.getElementById('password-form').value;
-  /* loginButton.addEventListener('click', () => { */
+  const loginButton = document.querySelector('#login-button');
+  loginButton.addEventListener('click', () => {
+    const loginEmail = document.querySelector('#email-form').value;
+    const loginPassword = document.querySelector('#password-form').value;
     if (loginEmail === 'tryber@teste.com' && loginPassword === '123456') {
       alert('Olá, Tryber!');
     } else {
       alert('Login ou senha inválidos.');
     }
-  /* }); */
+  });
 }
 loginAlert();
