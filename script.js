411dcbc01bb449567f6af81cd698960e874458d3
Uriel Bochi <urielbochi@gmail.com>
2021-04-23 23:50:42 -0300

    Projeto todo

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ const passUser = document.getElementById('password');
 const loginBtn = document.querySelector('#header__form--botao');
 
 function verifyLogin() {
-  if (loginUser.value === 'tryber@betrybe.com' && passUser.value === '123456') {
-    alert('Olá, Tryber!');
-  } else {
+  if (loginUser.value !== 'tryber@betrybe.com' && passUser.value !== '123456') {
     alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
 }
 
 loginBtn.addEventListener('click', verifyLogin);
+
+const btnAgreement = document.getElementById('agreement');
+
+function addProperty() {
+  const submitBtn = document.getElementById('submit-btn');
+  submitBtn.removeAttribute('disabled');
+}
+
+btnAgreement.addEventListener('click', addProperty);
