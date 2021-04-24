405f98301e02c5c23b65e85ea71edc4d670e9db2
Lais Simenikim <l.simenikim@gmail.com>
2021-04-24 18:05:59 -0300

    add requisito 18

---
+++ script.js
@@ -0,0 +1,13 @@
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+agreement.addEventListener('change', checkboxChecked);
+
+function checkboxChecked() {
+  //   if (agreement.checked === true) {
+  //     submitBtn.disabled = false;
+  //   } else {
+  //     submitBtn.disabled = true;
+  //   }
+  submitBtn.disabled = !agreement.checked;
+}
