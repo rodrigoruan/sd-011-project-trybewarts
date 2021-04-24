7b4e6c446d62647c051b1e99617c2079a381711c
Ghislaine <glenox79@gmail.com>
2021-04-24 12:40:51 -0300

    Corrigindo erros linter exer: 02 e 03

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function validateLoginPassword() {
   const login = document.querySelector('#inputEmail1');
   const password = document.querySelector('#inputPassword1');
   if (login.value !== 'tryber@teste.com' && password.value !== '123456') {
-    alert('Login ou senha inválidos');
+    alert('Login ou senha inválidos!');
   } else {
     alert('Olá, Tryber!');
     form1.value = '';
