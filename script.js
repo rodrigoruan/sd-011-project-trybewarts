1d791bf7202429459acddd04461d88f0d445c5ce
AlyssonSene <alysson_silva@outlook.com>
2021-04-24 08:47:00 -0300

    requisito 18 adicionado

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+window.onload = function() {
+  document.getElementById('submit-btn').disabled = true;
+}
+
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const loginButton = document.querySelector('#login-button');
+const checkMark = document.getElementById('agreement');
 
 function checkEmail() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -11,3 +16,16 @@ function checkEmail() {
 }
 
 loginButton.addEventListener('click', checkEmail);
+
+function checkBox() {
+  document.getElementById('agreement').onclick =  function(event) {
+    if (checkMark.checked) {
+      document.getElementById('submit-btn').disabled = false;
+    }
+    else {
+      document.getElementById('submit-btn').disabled = true;
+    }
+  }
+}
+
+checkBox();
