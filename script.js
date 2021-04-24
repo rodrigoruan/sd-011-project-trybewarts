132dff4bde4717464cf796470ceaac6506802587
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-24 02:43:53 -0300

    lint

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const loginButton = document.getElementById('login-button');
 // Dentro da funço que é executada ao clicar no botão, deve-se verificar se o email.
 // que esta no input de email é igual a tryber@teste.com e senha igual a 123456.
 loginButton.addEventListener('click', () => {
-  if (document.getElementById('login').value === 'tryber@teste.com' &&
-    document.getElementById('senha').value === '123456') {
+  if (document.getElementById('login').value === 'tryber@teste.com' 
+    && document.getElementById('senha').value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
 });
 
-//Criar constante que recebe o botao Submit
+// Criar constante que recebe o botao Submit
 const agreementCheck = document.getElementById('agreement');
 agreementCheck.addEventListener('click', () => {
   if (document.getElementById('agreement').checked === true) {
@@ -19,4 +19,4 @@ agreementCheck.addEventListener('click', () => {
   } else {
     document.getElementById('submit-btn').disabled = true;
   }
-})
+});
