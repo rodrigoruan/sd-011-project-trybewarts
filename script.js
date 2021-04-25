87450e47571906af574314a7c8195ae8acaa12dd
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-25 19:14:26 -0300

    Finaliza requisito 21

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const submitLoginBtn = document.getElementById('header-submit');
-const agreementCheckbox = document.getElementById('agreement');
-const formSubmitButton = document.getElementById('submit-btn');
+const evaluationForm = document.getElementById('evaluation-form');
 const commentBox = document.getElementById('textarea');
 const counterDiv = document.getElementById('counter');
+const agreementCheckbox = document.getElementById('agreement');
+const formSubmitButton = document.getElementById('submit-btn');
+const formData = {};
+
+function getFormData() {
+  const rawFormData = new FormData(evaluationForm);
+
+  for (const key of rawFormData.keys()) {
+    const value = rawFormData.getAll(key);
+
+    if (key !== 'subject') {
+      [formData[key]] = value;
+    } else {
+      formData[key] = value;
+    }
+  }
+}
+
+function submitForm(event) {
+  getFormData();
+
+  evaluationForm.innerHTML = '';
+  evaluationForm.innerHTML += `Nome: ${formData.firstName} ${formData.lastName}<br>`;
+  evaluationForm.innerHTML += `Email: ${formData.email}<br>`;
+  evaluationForm.innerHTML += `Casa: ${formData.house}<br>`;
+  evaluationForm.innerHTML += `Família: ${formData.family}<br>`;
+  evaluationForm.innerHTML += `Matérias: ${formData.subject.join(', ')}<br>`;
+  evaluationForm.innerHTML += `Avaliação: ${formData.rate}<br>`;
+  evaluationForm.innerHTML += `Observações: ${formData.commentBox}`;
+  evaluationForm.classList.add('break-text');
+
+  event.preventDefault();
+}
 
 function counterCaracteres() {
   counterDiv.innerHTML = `Caracteres disponiveis: ${500 - commentBox.value.length}`;
@@ -30,3 +62,4 @@ function verifiedValues() {
 submitLoginBtn.addEventListener('click', verifiedValues);
 agreementCheckbox.addEventListener('change', toggleFormSubmitButtonState);
 commentBox.oninput = counterCaracteres;
+evaluationForm.addEventListener('submit', submitForm);
