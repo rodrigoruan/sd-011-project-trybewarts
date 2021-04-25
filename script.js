4f6ce192e5a9f096606f70f3347ffbe8aee5753a
Oryange <oryangestrifezze@gmail.com>
2021-04-24 21:10:42 -0300

    Adiciona Requisito 20

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function validateLogin() {
 
 const loginButton = document.querySelector('#button-header');
 loginButton.addEventListener('click', validateLogin);
+
+
+//Referência: https://developer.mozilla.org/pt-BR/docs/Web/API/Document/keypress_event
+
+
+const textarea = document.getElementById('textarea');
+let maxContador = textarea.getAttribute('maxlength')
+
+let counter = document.getElementById('counter');
+counter.innerText = textarea.getAttribute('maxlength');
+
+
+
+function contaCaracteries() {  
+   
+  let campo = textarea.value.length;
+  let restante = maxContador - campo
+ 
+  counter.innerHTML = restante;
+
+}
+
+textarea.addEventListener('keyup', contaCaracteries);
+textarea.addEventListener('keydown', contaCaracteries);
