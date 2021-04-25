951b5fb43ea5a426447dcaf82a2540aad67d07aa
igordmf <igordanielmf@gmail.com>
2021-04-25 14:10:24 -0300

    Corrige erros linter

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // requisito 3
 
 // requisito 18
-let checkbox = document.getElementById('agreement');
-let submitBtn = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 
 function changeBtnStatus(event) {
-  if(event.target.checked === true) {
+  if (event.target.checked === true) {
     return (submitBtn.removeAttribute('disabled'));
   }
   return (submitBtn.setAttribute('disabled', true));
