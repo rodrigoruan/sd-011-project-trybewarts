f41041aa4632f3e65cf0e2e122631ca9130f7ae2
Laura Gusmao <laurargus@gmail.com>
2021-04-25 18:26:42 -0300

    Adiciona requisito 21 com problema na identificacao das materias

--- script.js
+++ script.js
@@ -41,3 +41,86 @@ function enableButton() {
   }
 }
 agreement.addEventListener('change', enableButton);
+
+const fistName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const email = document.getElementById('input-email');
+const house = document.getElementById('house');
+
+/**
+ * Essa funcao cria elementos P
+ */
+function newElement() {
+  const createP = document.createElement('p');
+  createP.className = 'result';
+  return createP;
+}
+
+/**
+ * Essa funcao cria um array com as opcoes de conteudo marcadas no formulario
+ * Ref: https://www.w3schools.com/js/js_array_methods.asp
+ */
+function subjects() {
+  const subjectsValues = document.querySelectorAll('input[class="subject"]:checked');
+  const arrSubjectsValues = [];
+  for (let index = 0; index < subjectsValues.length; index += 1) {
+    arrSubjectsValues.push(`${subjectsValues[index].value}`);
+  }
+  return arrSubjectsValues.join(', ');
+}
+
+/**
+ * Essa funcao retorna os valores preenchidos no formilario em objeto
+ * Ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelector#find_a_specific_element_with_specific_values_of_an_attribute
+ */
+function formData() {
+  const nameVal = fistName.value;
+  const lastNameVal = lastName.value;
+  const emailVal = email.value;
+  const houseVal = house.value;
+  const familyVal = document.querySelector('input[name="family"]:checked').value;
+  const rateVal = document.querySelector('input[name="rate"]:checked').value;
+  const commentVal = textarea.value;
+  const subjectsVal = subjects();
+
+  return { nameVal, lastNameVal, emailVal, houseVal, familyVal, subjectsVal, rateVal, commentVal };
+}
+
+/**
+ * Essa funcao cria elementos p para cada input do formulario contendo os valores preenchidos
+ * Ref: https://eslint.org/docs/rules/prefer-template
+ * https://www.w3schools.com/js/js_object_definition.asp
+ */
+function formDataElements() {
+  const formValues = formData();
+  const nameP = newElement();
+  nameP.innerText = `Nome: ${formValues.nameVal} ${formValues.lastNameVal}`;
+  const emailP = newElement();
+  emailP.innerText = `Email: ${formValues.emailVal}`;
+  const houseP = newElement();
+  houseP.innerText = `Casa: ${formValues.houseVal}`;
+  const familyP = newElement();
+  familyP.innerText = `Família: ${formValues.familyVal}`;
+  const subjectsP = newElement();
+  subjectsP.innerText = `Matérias: ${formValues.strSubjectsVal}`;
+  const rateP = newElement();
+  rateP.innerText = `Avaliação: ${formValues.rateVal}`;
+  const commentP = newElement();
+  commentP.innerText = `Observações: ${formValues.commentVal}`;
+  return [nameP, emailP, houseP, familyP, subjectsP, rateP, commentP];
+}
+
+/**
+ * Essa funcao impede que aconteca o evento default do botao submit e adiciona os elementos P criados
+ * anteriormente ao elemento form
+ */
+function appendFormData(event) {
+  event.preventDefault();
+  const children = formDataElements();
+  const evaluationForm = document.getElementById('evaluation-form');
+  for (let index = 0; index < children.length; index += 1) {
+    evaluationForm.appendChild(children[index]);
+  }
+}
+
+formButton.addEventListener('click', appendFormData);
