07f46de812bd008756b2f91e8754b279bd26e15c
Vitor <vitorguima@gmail.com>
2021-04-25 18:18:56 -0300

    requisito 21 feito ate casa escolhida

--- script.js
+++ script.js
@@ -60,31 +60,62 @@ function countCharacters() {
   });
 }
 
-// remove formulário
-function cleanFormChilds() {
-  const getForm = document.getElementById('evaluation-form'); 
 
-  getForm.innerHTML = '';
+// cria parágrafo com nome e sobrenome
+function returnNameValue() {
+  const getFormDiv = document.getElementById('evaluation-form');
+  const fullNameDiv = document.querySelector('.first-row');
+  const nameInput = document.getElementById('input-name');
+  const lastNameInput = document.getElementById('input-lastname');
+  const fullName = document.createElement('p');
+  fullName.innerText =  `Nome: ${nameInput.value} ${lastNameInput.value}`;
+
+  getFormDiv.appendChild(fullName);
+  fullNameDiv.parentNode.removeChild(fullNameDiv);
 }
+
+// cria parágrafo com email e casa selecionada
+function returnEmailHouseValue() {
+  const getFormDiv = document.getElementById('evaluation-form');
+  const emailHouseDiv = document.querySelector('.second-row');
+  const emailInput = document.getElementById('input-email');
+  const houseInput = document.getElementById('house');
+  const email = document.createElement('p');
+  email.innerText =  `Email: ${emailInput.value}`;
+  const house = document.createElement('p');
+  house.innerText = `Casa: ${houseInput.value}`;
+
+  getFormDiv.appendChild(email);
+  getFormDiv.appendChild(house);
+  emailHouseDiv.parentNode.removeChild(emailHouseDiv);
+}
+
+// cria parágrafo com família
+function returnFamilyValue() {
+  const getFormDiv = document.getElementById('evaluation-form');
+  const checkFamily = document.querySelector("input[type='radio'][name='family']:checked").value;
+  const returnFamily = document.createElement('p');
+  returnFamily.innerText = `Família: ${checkFamily}`;
+
+  getFormDiv.appendChild(returnFamily);
+
+}
+
+// cria parágrafo com matérias
+
 
 // altera elementos do formulário
 function changeFormChilds() {
   const getSubmitButton = document.getElementById('submit-btn');
+  
 
   getSubmitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    cleanFormChilds()
+    returnNameValue();
+    returnEmailHouseValue();
+    returnFamilyContentValue()
   });
 }
-
-// cria informação do campo nome
-function returnNameValue() {
-  const nameInput = document.getElementById('input-name');
-  const nameValue = nameInput.value;
-
-  return nameValue;
-}
-
 
 window.onload = function start() {
   validateAccess();
