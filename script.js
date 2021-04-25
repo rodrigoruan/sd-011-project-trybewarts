b450f01448d264bb08e14ea0975c1b8c0f326a85
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-25 16:35:58 -0300

    Remove array para a questao 21

--- script.js
+++ script.js
@@ -35,13 +35,3 @@ const displayCount = document.querySelector('#counter');
 textArea.addEventListener('keyup', () => {
   displayCount.innerText = 500 - textArea.value.length;
 });
-
-/** Código que resolve requisito 21 */
-const log = [
-  'Nome:',
-  'Email:',
-  'Casa:',
-  'Familia:',
-  'Matérias:',
-  'Avaliação:',
-  'Observações:'];
