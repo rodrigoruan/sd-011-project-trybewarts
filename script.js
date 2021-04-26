c0ff7333d36d0cf4b4639cbf5c7cb9abac415bfb
Alexandre <naitwa.alexandre@gmail.com>
2021-04-26 10:47:26 -0300

    17

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 const buttonLogin = document.getElementById('button-login');
 const loginInput = document.getElementById('login');
 const passwordInput = document.getElementById('password');
+const submitButton = document.getElementById('submit_btn');
+const agree = document.getElementById('agreement');
 // const labelRate = document.getElementById('label-rate');
 // Functions
 
@@ -15,3 +17,19 @@ function login() {
 }
 
 buttonLogin.addEventListener('click', login);
+
+// agreement type checkbox 
+agree.addEventListener('change', (e) => {
+  if (e.target.classList.contains('checked')) {
+    e.target.classList.remove('checked');
+  } else {
+    e.target.classList.add('checked');
+  }
+});
+
+// submit form button
+submitButton.addEventListener('click', (e) => {
+  if (!agree.classList.contains('checked')) {
+    e.preventDefault();
+  }
+});
