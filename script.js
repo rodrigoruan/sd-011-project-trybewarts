5b9a82a8a7092b26af41fdf03a73c6eb5e4076bf
igordmf <igordanielmf@gmail.com>
2021-04-26 19:50:33 -0300

    Inicia requisito 21

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ loginButton.addEventListener('click', sendLogin);
 // requisito 18
 const checkbox = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
-
 function changeBtnStatus(event) {
   if (event.target.checked === true) {
     return (submitBtn.removeAttribute('disabled'));
@@ -35,3 +34,41 @@ function counterCharacters() {
 textArea.addEventListener('input', counterCharacters);
 
 // requisito 21
+function getInfoDiv1() {
+  const fName = document.getElementById('input-name').value;
+  const lName = document.getElementById('input-lastname').value;
+  const email = document.getElementById('input-email').value;
+  const house = document.getElementById('house').value;
+}
+function getInfoDiv2() {
+
+  // identificando a familia
+  const family = document.getElementsByClassName('family');
+  const familyArray = Array.from(family);
+  let selectedFamily = familyArray.filter((elemento) => elemento.checked === true);
+  selectedFamily = selectedFamily[0].value;
+
+  // identificando o conteúdo
+  const subject = document.getElementsByClassName('subject');
+  const subjectArray = Array.from(subject);
+  let subjectChecks = subjectArray.filter((elemento) => elemento.checked === true);
+  subjectChecks = subjectChecks.map((elemento) => elemento.value);
+}
+function getInfoDiv3() {
+
+  // identificando a nota
+  const rate = document.getElementsByClassName('rate');
+  const rateArray = Array.from(rate);
+  let rateValue = rateArray.filter((elemento) => elemento.checked === true);
+  rateValue = rateValue[0].value;
+
+  // identificando o comentário
+  const notes = document.getElementById('textarea').value;
+}
+function changeForms(event) {
+  event.preventDefault();
+  getInfoDiv1();
+  getInfoDiv2();
+  getInfoDiv3();
+}
+submitBtn.addEventListener('click', changeForms)
