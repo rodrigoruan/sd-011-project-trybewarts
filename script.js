742c986a297033784cc9a0a1dae209d7be16cdb9
Dario Junior <dario.junior.rs@gmail.com>
2021-04-26 19:11:42 -0300

    resolve 100%

--- script.js
+++ script.js
@@ -30,19 +30,65 @@ textArea.addEventListener('keyup', () => {
 });
 
 btnSubmit.addEventListener('click', (event) => {
-  const fieldName = ([pl, ...resto]) => pl.toUpperCase() + resto.join('').toLowerCase();
+  const fields = {
+    1: 'Nome',
+    2: 'Sobrenome',
+    3: 'Email',
+    4: 'Casa',
+    5: 'Família',
+    6: 'Matérias',
+    7: 'Avaliação',
+    8: 'Observações'
+  }
+
   fieldsetInformations.style.display = 'none';
   event.preventDefault();
-  const target = event.path[3];
-  for (let index = 1; index < event.path.length; index += 1) {
+
+  const target = event.path[2];
+  for (let index = 1; index < event.path[2].children.length; index += 1) {
     const pText = document.createElement('p');
     mainForm.appendChild(pText);
-    if (target[index].name === 'nome') {
-      pText.innerText = `${fieldName(target[index].name)}: 
-      ${target[index].value} ${target[index + 1].value}`;
+
+    if (target.children[index].name === 'nome') {
+      pText.innerText = `${fields[index]}: ${target.children[index].value} ${target.children[index + 1].value} `;
       index += 1;
-    } else {
-      pText.innerText = ` ${fieldName(target[index].name)}: ${target[index].value}`;
+    }
+
+    if (target.children[index].name === 'email') {
+      pText.innerText = `Email: ${target.children[index].value}`
+    }
+
+    if (target.children[index].name === 'casa') {
+      pText.innerText = `Casa: ${target.children[index].value}`
+    }
+
+    if (target.children[index].dataset.name === 'family') {
+      let rangeField = target.children[index].children;
+      for (let family of rangeField) {
+        if (family.checked) pText.innerText = `${fields[5]}: ${family.value}`;
+      }
+    }
+
+    if (target.children[index].dataset.name === 'tech') {
+      let rangeField = target.children[index].children;
+      let techs = [];
+      pText.innerText = `${fields[6]}: `
+      for (let tech of rangeField) {
+        if (tech.checked) techs.push(tech.value);
+      }
+      pText.innerText += ` ${techs.join(', ')}`;
+    }
+
+    if (target.children[index].id == 'rateValue') {
+      let rangeField = target.children[index].children;
+      pText.innerText = `${fields[7]}: `
+      for (let rate of rangeField) {
+        if (rate.checked) pText.innerText += ` ${rate.value}`;
+      }
+    }
+
+    if (target.children[index].id === 'feedbackArea') {
+      pText.innerText = `${fields[8]}: ${target.children[index].children[1].value}`
     }
   }
   mainForm.style.display = 'block';
