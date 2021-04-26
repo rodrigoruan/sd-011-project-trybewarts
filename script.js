3012acdd29810ac34161e3a289eef3355131b4c0
Renan <renan.vamo@gmail.com>
2021-04-26 11:48:23 -0300

    Adiciona alguns parametros do ex 21

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Declara variáveis comuns para as diferentes funções
 const submitBtn = document.getElementById('submit-btn');
+const inputName = document.querySelector('#input-name');
+const inputLastN = document.querySelector('#input-lastname');
+const inputEmail = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+
+const subject = document.querySelectorAll('.subject').checked;
+const evaluatorRate = document.querySelectorAll('#label-rate').checked;
+const textArea = document.querySelector('#textarea');
 
 // Login do usuário
 function CheckLogin() {
@@ -9,8 +17,6 @@ function CheckLogin() {
   loginBtn.addEventListener('click', () => {
     const login = document.querySelector('#login').value;
     const password = document.querySelector('#password').value;
-    console.log(login);
-    console.log(password);
     if (login === validLogin && password === validPw) {
       alert('Olá, Tryber!');
     } else {
@@ -47,6 +53,17 @@ function CharacterLimit() {
   const tMaxLength = document.getElementById('textarea').maxLength;
   const pCounter = document.getElementById('counter');
   pCounter.innerHTML = tMaxLength - counter;
+}
+
+function FillInputs() {
+  const form = document.getElementsByTagName('form')[1];
+  form.innerHTML = '';
+  form.innerHTML += `Nome: ${inputName.value} ${inputLastN.value} <br>`;
+  form.innerHTML += `Email: ${inputEmail.value} <br>`;
+  form.innerHTML += `Casa: ${house.value} <br>`;
+  form.innerHTML += `Família: <br>`;
+  console.log(document.querySelector('input[class="family"]:checked'));
+
 }
 
 // Função que chama todas as funções do site
