1aece3855c32158c4c1a82136d74f9083e8330a9
Antonio Malato <malatoneto@gmail.com>
2021-04-25 23:23:49 -0300

    Correcao Lint

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const password = document.getElementById('password');
 const button = document.getElementById('btn-input');
 
 function getInput() {
-  if ((login.value === 'tryber@teste.com') && (password.value === '123456')) {
-    alert('Olá, Tryber!');
-  } else {
+  if (login.value !== 'tryber@teste.com' || password.value !== '123456') {
     alert('Login ou senha inválidos.');
+  } else {
+    alert('Olá, Tryber!');
   }
 }
 button.addEventListener('click', getInput);
