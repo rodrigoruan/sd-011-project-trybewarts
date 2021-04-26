b638a4ea4ac59350e3cc02ac19cae2a1eb647f9f
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-25 19:10:28 -0700

    Correções

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function handleAgreement(event) {
 }
 
 function countCaracter() {
-  const textEntered = document.getElementById('text-area').value;
+  const textEntered = document.getElementById('textarea').value;
   const counter = (500 - textEntered.length);
   const countRemaining = document.getElementById('counter');
   countRemaining.textContent = counter;
@@ -27,21 +27,32 @@ function countCaracter() {
 
 function handleChangeFullName() {
   const nameElement = document.getElementById('input-name');
-  const nameFilled = document.getElementById('input-name').value;
+  const nameFilled = nameElement.value;
   const lastNameFilled = document.getElementById('input-lastname').value;
-  const divFlex = nameElement.parentNode;
-  divFlex.innerHTML = `Nome: ${nameFilled} ${lastNameFilled}`;
+  const divRow1 = nameElement.parentNode;
+  divRow1.innerHTML = `Nome: ${nameFilled} ${lastNameFilled}`;
+}
+
+function handleChangeEmail() {
+  const emailElement = document.getElementById('input-email');
+  const emailFilled = emailElement.value;
+  const divRow2 = emailElement.parentNode;
+  divRow2.innerHTML = `Email: ${emailFilled}`;
 }
 
 function handleChanges() {
   handleChangeFullName();
+  handleChangeEmail();
 }
 
 const button = document.getElementById('botao');
 button.addEventListener('click', handleLogin);
+
 const agreement = document.getElementById('agreement');
 agreement.addEventListener('change', handleAgreement);
-const textAreaElement = document.getElementById('text-area');
+
+const textAreaElement = document.getElementById('textarea');
 textAreaElement.addEventListener('keyup', countCaracter);
+
 const submitButton = document.getElementById('submit-btn');
 submitButton.addEventListener('click', handleChanges);
