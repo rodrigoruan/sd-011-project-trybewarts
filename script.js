2fdcf2fa1597b2eb55303528c84c1cddc68c714f
Luana Souza <luanah.souzza@hotmail.com>
2021-04-25 22:20:53 -0300

    Corrige erro Eslint

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const loginInput = document.getElementById('id');
 const senhaInput = document.getElementById('password');
 const button = document.getElementById('submit-button');
 
-
-
 button.addEventListener('click', () => {
   if (loginInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
     alert('Olá, Tryber!');
