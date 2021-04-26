9f2f00545ecc99b850c867b525cad2b7a71433b6
Alexandre <naitwa.alexandre@gmail.com>
2021-04-26 14:37:51 -0300

    quase terminado

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const loginInput = document.getElementById('login');
 const passwordInput = document.getElementById('password');
 const submitButton = document.getElementById('submit-btn');
 const agree = document.getElementById('agreement');
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputHouse = document.getElementById('house');
+const inputTextArea = document.getElementById('textarea');
+const forms = document.getElementById('evaluation-form');
 // const labelRate = document.getElementById('label-rate');
 // Functions
 
@@ -22,6 +28,8 @@ submitButton.disabled = true;
 agree.addEventListener('change', (e) => {
   if (e.target.checked) {
     submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
   }
 });
 
@@ -30,7 +38,38 @@ function addCounter() {
   const textArea = document.getElementById('textarea');
   textArea.addEventListener('keyup', (e) => {
     const counter = document.getElementById('counter');
-    counter.innerHTML = `${500 - e.target.value.length} caracteres restantes`;
+    counter.innerHTML = `${e.target.maxLength - e.target.value.length} caracteres restantes`;
   });
 }
 addCounter();
+
+function materiasChecked() {
+  const inputChecked = document.querySelectorAll('.subject:checked');
+  const inputValue = [];
+  for (let index = 0; index < inputChecked.length; index += 1) {
+    inputValue.push(` ${inputChecked[index].value}`);
+  }
+  return inputValue;
+}
+
+submitButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  const inputs = {
+    Nome: `${inputName.value} ${inputLastName.value}`,
+    Email: inputEmail.value,
+    Casa: inputHouse.value,
+    Família: document.querySelector('input[name="family"]:checked').value,
+    Matérias: materiasChecked(),
+    Avaliação: document.querySelector('input[name="rate"]:checked').value,
+    Observações: inputTextArea.value,
+  };
+
+  let inputForm = '';
+  for (const i of Object.keys(inputs)) {
+    inputForm += `${i}: ${inputs[i]} <div>`;
+    e.preventDefault();
+  }
+  console.log(forms);
+  forms.innerHTML = inputForm;
+  e.preventDefault();
+});
