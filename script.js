7bc35734b28d49214eed0c56b64ea81d2c412ea9
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-26 19:28:16 -0300

    Requisito 21 - finalizando projeto

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const buttonLogin = document.getElementById('buttonLogin');
 const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
+const checkbox = document.getElementById('agreement');
+const submit = document.getElementById('submit-btn');
+const inputName = document.getElementById('input-name');
+const inputLastName = document.getElementById('input-lastname');
+const inputCurrentEmail = document.getElementById('input-email');
+const house = document.getElementById('house');
+const text = document.querySelector('textarea');
+const form = document.getElementById('evaluation-form');
 
 buttonLogin.addEventListener('click', () => {
   const inputEmail = document.getElementById('loginEmail').value;
@@ -13,8 +21,7 @@ buttonLogin.addEventListener('click', () => {
   }
 });
 // 18
-const checkbox = document.getElementById('agreement');
-const submit = document.getElementById('submit-btn');
+
 submit.disabled = true;
 
 checkbox.addEventListener('click', () => {
@@ -27,4 +34,23 @@ checkbox.addEventListener('click', () => {
 
 textArea.addEventListener('keyup', () => {
   counter.innerHTML = 500 - textArea.value.length;
+});
+
+submit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const rate = document.querySelector('input[name="rate"]:checked').value;
+  const check = document.querySelectorAll('.subject:checked');
+  const arrChecked = [];
+  for (let index = 0; index < check.length; index += 1) {
+    arrChecked.push(check[index].value);
+  }
+
+  form.innerHTML += `Nome: ${inputName.value} ${inputLastName.value}
+  Email: ${inputCurrentEmail.value}
+  Casa: ${house.value}
+  Família: ${family}
+  Matérias: ${arrChecked.join(', ')}
+  Avaliação: ${rate}
+  Observações: ${text.value}`;
 });
