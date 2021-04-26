0217532f3d00e71b2505d8ddca6a2047de3b8a84
Daniel <danielfdoficial@gmail.com>
2021-04-26 12:05:01 -0300

    adiciona toogle Attribute

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const login = document.getElementById('login');
 const password = document.getElementById('password');
 const commentTextarea = document.getElementById('textarea');
 const commentCounter = document.getElementById('counter');
+const checkAgreements = document.querySelector('#label-infos');
+const submitButton = document.querySelector('#submit-btn');
+const agreement = document.getElementById('agreement');
 
 function validateLogin() {
   if (login.value + password.value !== 'tryber@teste.com123456') {
@@ -17,7 +20,25 @@ function counter() {
   commentCounter.textContent = 500 - commentLength;
 }
 
+/* function addClass() {
+    checkAgreements.classList.add('checked');
+}
+
+function removeClass() {
+  if (checkAgreements.className === 'checked') {
+    checkAgreements.classList.remove('checked');
+  }
+} */
+
+function checkClass() {
+  
+}
+
 // eventLissiners
 
 commentTextarea.addEventListener('keyup', counter);
-button.addEventListener('click', validateLogin);
+button.addEventListener('click', validateLogin);/* 
+checkAgreements.addEventListener('click', checkClass); */
+agreement.addEventListener('click', () => {
+  submitButton.toggleAttribute('disabled');
+});
