68c890887eb6c60047b141c6a34cbbd644076c2c
Dario Junior <dario.junior.rs@gmail.com>
2021-04-27 15:01:31 -0300

    100% de tudo pelo amor de santo cristo

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const btnLogin = document.querySelector('#submitLogin');
-const checkBox = document.querySelector('#agreement');
-const btnSubmit = document.querySelector('#submit-btn');
-const textArea = document.querySelector('#textarea');
-const counter = document.querySelector('#counter');
+
 const mainForm = document.querySelector('#evaluation-form');
 const fieldsetInformations = document.querySelector('#field-set');
+
+const textArea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+
+const checkBox = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
+
+const fields = {
+  Nome: document.querySelector('#input-name'),
+  Sobrenome: document.querySelector('#input-lastname'),
+  Email: document.querySelector('#input-email'),
+  Casa: document.querySelector('#house'),
+  Família: document.querySelectorAll('#label-family ~ input.family'),
+  Matérias: document.querySelectorAll('#label-content ~ input.content'),
+  Avaliação: document.querySelectorAll('#label-rate ~input.rate'),
+  Observações: document.querySelector('#textarea'),
+};
 
 btnLogin.addEventListener('click', (event) => {
   const eventPath = event.path;
@@ -29,67 +43,38 @@ textArea.addEventListener('keyup', () => {
   counter.innerText = 500 - textArea.value.length;
 });
 
-btnSubmit.addEventListener('click', (event) => {
-  const fields = {
-    1: 'Nome',
-    2: 'Sobrenome',
-    3: 'Email',
-    4: 'Casa',
-    5: 'Família',
-    6: 'Matérias',
-    7: 'Avaliação',
-    8: 'Observações'
+function checkedValues(elem) {
+  const checkedItems = [];
+  for (const item of elem) {
+    if (item.checked) checkedItems.push(item.value);
   }
+  return checkedItems.join(', ');
+}
 
-  fieldsetInformations.style.display = 'none';
+function createrElement(elem, fieldName) {
+  const pText = document.createElement('p');
+  pText.className = `${fieldName.toLowerCase()}`
+  pText.innerText = `${fieldName}: `;
+
+  if (elem.length > 0) {
+    if (elem.nodeName === 'SELECT') pText.innerText += ` ${elem.value}`;
+    pText.innerText += `${checkedValues(elem)}`;
+  } else if (fieldName === 'Sobrenome') {
+    const p = document.querySelector('p.nome');
+    p.innerText += ` ${elem.value}`;
+  } else {
+    pText.innerText += elem.value;
+  }
+  return mainForm.appendChild(pText);
+}
+
+btnSubmit.addEventListener('click', (event) => {
   event.preventDefault();
-
-  const target = event.path[2];
-  for (let index = 1; index < event.path[2].children.length; index += 1) {
-    const pText = document.createElement('p');
-    mainForm.appendChild(pText);
-
-    if (target.children[index].name === 'nome') {
-      pText.innerText = `${fields[index]}: ${target.children[index].value} ${target.children[index + 1].value} `;
-      index += 1;
-    }
-
-    if (target.children[index].name === 'email') {
-      pText.innerText = `Email: ${target.children[index].value}`
-    }
-
-    if (target.children[index].name === 'casa') {
-      pText.innerText = `Casa: ${target.children[index].value}`
-    }
-
-    if (target.children[index].dataset.name === 'family') {
-      let rangeField = target.children[index].children;
-      for (let family of rangeField) {
-        if (family.checked) pText.innerText = `${fields[5]}: ${family.value}`;
-      }
-    }
-
-    if (target.children[index].dataset.name === 'tech') {
-      let rangeField = target.children[index].children;
-      let techs = [];
-      pText.innerText = `${fields[6]}: `
-      for (let tech of rangeField) {
-        if (tech.checked) techs.push(tech.value);
-      }
-      pText.innerText += ` ${techs.join(', ')}`;
-    }
-
-    if (target.children[index].id == 'rateValue') {
-      let rangeField = target.children[index].children;
-      pText.innerText = `${fields[7]}: `
-      for (let rate of rangeField) {
-        if (rate.checked) pText.innerText += ` ${rate.value}`;
-      }
-    }
-
-    if (target.children[index].id === 'feedbackArea') {
-      pText.innerText = `${fields[8]}: ${target.children[index].children[1].value}`
-    }
+  fieldsetInformations.style.display = 'none';
+  for (const fieldName of Object.keys(fields)) {
+    const fieldValue = fields[`${fieldName}`];
+    console.log(fieldName, fieldValue.nodeName, fieldValue)
+    createrElement(fieldValue, fieldName);
   }
   mainForm.style.display = 'block';
 });
