271ec73808f3931e2a6b7ca6c4fac170e760072d
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-27 15:31:28 -0300

    21 - substitui o formulario pelos dados do formulario

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // Criar um addEventListener no botão LOGAR.
-const loginButton = document.getElementById('login-button');
+const loginButton = document.getElementById("login-button");
 // Dentro da funçao que é executada ao clicar no botão, deve-se verificar se o email.
 // que esta no input de email é igual a tryber@teste.com e senha igual a 123456.
-loginButton.addEventListener('click', () => {
-  if (document.getElementById('login').value !== 'tryber@teste.com'
-    && document.getElementById('senha').value !== '123456') {
-    alert('Login ou senha inválidos.');
+loginButton.addEventListener("click", () => {
+  if (
+    document.getElementById("login").value !== "tryber@teste.com" &&
+    document.getElementById("senha").value !== "123456"
+  ) {
+    alert("Login ou senha inválidos.");
   } else {
-    alert('Olá, Tryber!');
+    alert("Olá, Tryber!");
   }
 });
 
 // Criar constante que recebe o botao Submit
-const agreementCheck = document.getElementById('agreement');
-const submitButton = document.getElementById('submit-btn');
-agreementCheck.addEventListener('click', () => {
-  if (document.getElementById('agreement').checked === true) {
+const agreementCheck = document.getElementById("agreement");
+const submitButton = document.getElementById("submit-btn");
+agreementCheck.addEventListener("click", () => {
+  if (document.getElementById("agreement").checked === true) {
     submitButton.disabled = false;
   } else {
     submitButton.disabled = true;
@@ -23,35 +25,51 @@ agreementCheck.addEventListener('click', () => {
 });
 
 // Contador
-const counter = document.getElementById('counter');
-const textArea = document.getElementById('textarea');
+const counter = document.getElementById("counter");
+const textArea = document.getElementById("textarea");
 let tamanho = 500;
-textArea.addEventListener('keyup', () => {
-// Diminuir do counter o tamanho atual do textos
-  tamanho = 500 - (textArea.value.length);
+textArea.addEventListener("keyup", () => {
+  // Diminuir do counter o tamanho atual do textos
+  tamanho = 500 - textArea.value.length;
   counter.innerHTML = tamanho;
 });
 
-submitButton.addEventListener('click', () => {
-  // guardar no local storage rs
-  localStorage.setItem('name', document.getElementById('input-name').value);
-  localStorage.setItem('lastname', document.getElementById('input-lastname').value);
-  localStorage.setItem('email', document.getElementById('input-email').value);
-  localStorage.setItem('house', document.getElementById('house').value);
-  localStorage.setItem('familia', document.querySelector('input[name="family"]:checked').value);
-  localStorage.setItem('materia', document.querySelector('input[name="materia"]:checked').value);
-  localStorage.setItem('avalia', document.querySelector('input[name="rate"]:checked').value);
-  localStorage.setItem('obs', document.getElementById('textarea').value);
+// guardar variáveis (dados do formulário)
+// Apagar formulário ao recarregar página
+// Imprimir informações guardadas no formulário
+const evaluationForm = document.forms.namedItem("evaluation-form");
+evaluationForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let nome = document.getElementById("input-name").value;
+  let sobrenome = document.getElementById("input-lastname").value;
+  let email = document.getElementById("input-email").value;
+  let casa = document.getElementById("house").value;
+  let familia = document.querySelector('input[name="family"]:checked').value;
+  let materias = "";
+  for (var index = 0; index < 6; index++) {
+    if (document.getElementsByClassName("subject")[index].checked) {
+      // if verifica se a propriedade checked do elemento atual da lista de elementos que possuem a classe (subject) está ativa ou seja,  true
+      // se for então
+      materias += document.getElementsByClassName("subject")[index].value + " ";
+    }
+  }
+  let avaliacao = document.querySelector('input[name="rate"]:checked').value;
+  let observacoes = document.getElementById("textarea").value;
+
+  removeForm();
+
+  console.log(materias);
+  evaluationForm.innerHTML = `Nome: ${nome} ${sobrenome} <br>
+  Email: ${email} <br>
+  Casa: ${casa} <br>
+  Família: ${familia} <br>
+  Matérias: ${materias} <br>
+  Avaliação: ${avaliacao} <br>
+  Observações: ${observacoes}`;
 });
-// exibir nos campos o conteúdo coletado?
-// window.onload = () => {
-//   // colocando nos campos o que guardou em variáveis
-//   document.getElementById('input-name').value = localStorage.getItem('name');
-//   document.getElementById('input-lastname').value = localStorage.getItem('lastname');
-//   document.getElementById('input-email').value = localStorage.getItem('email');
-//   document.getElementById('house').value = localStorage.getItem('house');
-//   document.querySelector(`input[value=${localStorage.getItem('familia')}]`).checked = true;
-//   document.querySelector(`input[value=${localStorage.getItem('materia')}]`).checked = true;
-//   document.querySelector(`input[value="${localStorage.getItem('avalia')}"]`).checked = true;
-//   document.getElementById('textarea').value = localStorage.getItem('obs');
-// };
+
+function removeForm() {
+  for (let i = 0; i < 5; i += 1) {
+    evaluationForm.lastElementChild.remove();
+  }
+}
