4a170d0af42b85ca22a64ea21db1cf664c15c84c
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-27 20:45:21 -0300

    test pull

--- script.js
+++ script.js
@@ -4,41 +4,44 @@ const inptPassword = document.getElementById('password');
 const btnCheck = document.getElementById('agreement');
 const btnSub = document.getElementById('submit-btn');
 
-function LoginAlert() {
-  if (inputLogin.value === 'tryber@teste.com' && inptPassword.value === '123456') {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Login ou senha inválidos.');
-  }
+const LoginAlert =() => {
+if (inputLogin.value === 'tryber@teste.com' && inptPassword.value === '123456') {
+alert('Olá, Tryber!');
+} else {
+alert('Login ou senha inválidos.');
+}
 }
 
 btnLogin.addEventListener('click', LoginAlert);
 
 btnSub.disabled = true;
 
-function validateButton() {
-  if (btnCheck.checked === false) {
-    btnSub.disabled = true;
-  } else {
-    btnSub.disabled = false;
-  }
+const validateButton =() => {
+if (btnCheck.checked === false) {
+btnSub.disabled = true;
+} else {
+btnSub.disabled = false;
+}
 }
 
 btnCheck.addEventListener('click', validateButton);
 
 // tentando desenrolar o requisito 21!
-// const forms = document.getElementById('evaluation-form');
-// const nameInput = document.getElementById('input-name');
-// const lastNameInput = document.getElementById('input-lastname');
-// const emailInput = document.getElementById('input-email');
-// const houseInput = document.getElementById('house');
-// const familyInput = document.getElementsByClassName('family');
-// const subjectInput = document.getElementsByClassName('subject');
-// const radioInput = document.getElementsByClassName('radioRate');
+const forms = document.getElementById('evaluation-form');
+const nameInput = document.getElementById('input-name');
+const lastNameInput = document.getElementById('input-lastname');
+const emailInput = document.getElementById('input-email');
+const houseInput = document.getElementById('house');
+const familyInput = document.getElementsByClassName('family');
+const subjectInput = document.getElementsByClassName('subject');
+const radioInput = document.getElementsByClassName('radioRate');
+const texts = document.querySelector('#textarea');
+const conter = document.getElementById('counter');
+
 // function createParagraphs(array) {
-//   for (let i = 0; i < array.length; i += 1) {
-//     const p = document.createElement('p');
-//     p.innerText = array[i];
-//     forms.appendChild(p);
-//   }
+// for (let i = 0; i < array.length; i += 1) {
+// const p = document.createElement('p');
+// p.innerText = array[i];
+// forms.appendChild(p);
+// }
 // }
