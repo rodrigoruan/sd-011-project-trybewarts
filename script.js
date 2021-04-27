3e16300dc9e97b5246e1ccb6398e50c89c21ac52
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-27 10:56:42 -0300

    JavaScript refatorado

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const loginButton = document.querySelector('#login-button');
 const checkValue = document.getElementById('agreement');
-const textarea = document.querySelector('#textarea');
-const counter = document.querySelector('#counter');
-counter.innerText = 500;
+const submitBtn = document.querySelector('#submit-btn');
+
+[loginButton, checkValue, submitBtn].forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (item === loginButton) {
+      loginValidation();
+    } else if (item === checkValue) {
+      agreement();
+    } else if (item === submitBtn) {
+      getDataForm(event);
+    }
+  });
+});
 
 function loginValidation() {
   const trybewartsLogin = document.forms['trybewarts-login'];
@@ -23,69 +33,27 @@ function agreement() {
   }
 }
 
-[loginButton, checkValue].forEach((item) => {
-  item.addEventListener('click', () => {
-    if (item === loginButton) {
-      loginValidation();
-    } else if (item === checkValue) {
-      agreement();
-    }
-  });
-});
-
-textarea.addEventListener('keyup', () => {
-  const current = textarea.value.split('').length;
-  counter.innerText = 500 - current;
-});
-
-const submitBtn = document.querySelector('#submit-btn');
 const fName = document.querySelector('#input-name');
 const lName = document.querySelector('#input-lastname');
-const eMail = document.querySelector('#input-email');
+const email = document.querySelector('#input-email');
 const house = document.querySelector('#house');
-const family = document.getElementsByName('family');
-const subject = document.getElementsByName('checkbox-content');
-const score = document.getElementsByName('rate');
 const observations = document.querySelector('#textarea');
-console.log(family.length);
 
-function getFamily() {
-  for (let index = 0; index < family.length; index += 1) {
-    if (family[index].checked) {
-      return family[index].value;
-    }
-  }
-}
-
-function getSubject() {
-  for (let index = 0; index < subject.length; index += 1) {
-    if (subject[index].checked) {
-      return subject[index].value;
-    }
-  }
-}
-
-function getScore() {
-  for (let index = 0; index < score.length; index += 1) {
-    if (score[index].checked) {
-      return score[index].value;
-    }
-  }
-}
-
-function getDataForm(e) {
-  e.preventDefault();
+function getDataForm(event) {
+  event.preventDefault();
   const object = {
     name: `Nome: ${fName.value} ${lName.value}`,
-    email: `Email: ${eMail.value}`,
+    email: `Email: ${email.value}`,
     house: `Casa: ${house.value}`,
     family: `Família: ${getFamily()}`,
     subject: `Matérias: ${getSubject()}`,
     score: `Avaliação: ${getScore()}`,
     observations: `Observações: ${observations.value}`,
   };
-
-  const formFilled = document.querySelector('#form-filled');
+  const mainForm = document.querySelector('#evaluation-form');
+  const formFilled = document.createElement('div');
+  formFilled.id = 'form-filled';
+  mainForm.appendChild(formFilled);
 
   for (const key in object) {
     if (object[key] !== 0) {
@@ -96,4 +64,41 @@ function getDataForm(e) {
   }
 }
 
-submitBtn.addEventListener('click', getDataForm);
+const family = document.getElementsByName('family');
+
+function getFamily() {
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked) {
+      return family[index].value;
+    }
+  }
+}
+
+const subject = document.getElementsByName('checkbox-content');
+
+function getSubject() {
+  for (let index = 0; index < subject.length; index += 1) {
+    if (subject[index].checked) {
+      return subject[index].value;
+    }
+  }
+}
+
+const score = document.getElementsByName('rate');
+
+function getScore() {
+  for (let index = 0; index < score.length; index += 1) {
+    if (score[index].checked) {
+      return score[index].value;
+    }
+  }
+}
+
+const textarea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
+counter.innerText = 500;
+
+textarea.addEventListener('keyup', () => {
+  const current = textarea.value.split('').length;
+  counter.innerText = 500 - current;
+});
