d04c165e2d6c62284c1ca79d79f1f06f3c41ae28
Maran <maranmorimoto@gmail.com>
2021-04-26 22:39:39 -0300

    tentando funcionar Requisito 21

--- script.js
+++ script.js
@@ -43,32 +43,51 @@ const fName = document.querySelector('#input-name');
 const lName = document.querySelector('#input-lastname');
 const eMail = document.querySelector('#input-email');
 const house = document.querySelector('#house');
-const family = document.querySelector('#label-family');
+const family = document.getElementsByName('family');
+const subject = document.getElementsByName('checkbox-content');
+const score = document.getElementsByName('rate');
+const observations = document.querySelector('#textarea');
+console.log(family.length);
+
+function getFamily() {
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked);
+    return family[index].value;
+  }
+}
+
+function getSubject() {
+  for (let index = 0; index < subject.length; index += 1) {
+    if (subject[index].checked);
+    return subject[index].value;
+  }
+}
+
+function getScore() {
+  for (let index = 0; index < score.length; index += 1) {
+    if (score[index].checked);
+    return score[index].value;
+  }
+}
 
 function getDataForm(e) {
   e.preventDefault();
   let object = {
     name: `Nome: ${fName.value} ${lName.value}`,
     email: `Email: ${eMail.value}`,
-    house: `Casa: `,
-    family: `Família: `,
-    subjects: `Matérias: `, 
-    score: `Avaliação: `,
-    observations: `Observações: `
-  }
-  
+    house: `Casa: ${house.value}`,
+    family: `Família: ${getFamily()}`,
+    subject: `Matérias: ${getSubject()}`,
+    score: `Avaliação: ${getScore()}`,
+    observations: `Observações: ${observations.value}`,
+  };
+
   const keys = Object.keys(object);
   for (let key in object) {
     const paramForm = document.createElement('p');
     paramForm.innerText = object[key];
     formFilled.appendChild(paramForm);
   }
-  // for (let index = 0; index < keys.length; index += 1) {
-  //   let currentKey = keys[index];
-  //   paramForm.innerText = object.currentKey;
-  //   formFilled.appendChild(paramForm);
-  // }
-
 }
 
 submitBtn.addEventListener('click', getDataForm);
