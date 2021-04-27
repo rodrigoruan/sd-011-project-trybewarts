d7fed60595345b6148852c70f206c70abe37c580
Alan Gonçalves <vekio.soft@gmail.com>
2021-04-27 20:22:51 -0300

    correção de espaços no srcipt.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-//formulario
+// Formulario
 const buttonLogin = document.getElementById('btn-login');
-const nome = document.getElementById('input-name');
-const sobreNome = document.getElementById('input-lastname');
-const email = document.getElementById('input-email');
-const casa = document.getElementById('house')
-const familia = document.getElementById('label-family');
-const materia = document.getElementById('label-content');
-const avaliation = document.getElementById('label-rate');
 const sendBtn = document.getElementById('submit-btn');
 const mainForm = document.getElementById('main-form');
 const forms = document.getElementById('evaluation-form');
@@ -44,7 +37,6 @@ function counterText() {
 
 textField.addEventListener('keyup', counterText);
 
-
 // 21. Faça com que ao clicar no botão 'Enviar', o conteúdo do formulário seja substituído pelas informações preenchidas
 
 // Ao clicar no botão de enviar, existe um texto no formato Nome: -Nome- -Sobrenome-
@@ -74,17 +66,17 @@ function houses() {
 
 // Ao clicar no botão de enviar, existe um texto no formato Família: -Família-
 function families() {
-  const getInput = document.querySelectorAll('input');
+  const input = document.querySelectorAll('input');
   const getDiv4 = document.createElement('div');
-  for (let index = 0; index < getInput.length; index += 1) {
-    if (getInput[index].type === 'radio' && getInput[index].name === 'family' && getInput[index].checked) {
-      getDiv4.innerHTML = `Família: ${getInput[index].value}`;
+  for (let index = 0; index < input.length; index += 1) {
+    if (input[index].type === 'radio' && input[index].name === 'family' && input[index].checked) {
+      getDiv4.innerHTML = `Família: ${input[index].value}`;
     }
   }
   return getDiv4;
 }
 
-//Ao clicar no botão de enviar, existe um texto no formato Matérias: -Matérias Selecionadas-
+// Ao clicar no botão de enviar, existe um texto no formato Matérias: -Matérias Selecionadas-
 function familyBox() {
   const getFamily = document.querySelectorAll('input[class="subject"]:checked');
   const getDiv5 = document.createElement('div');
@@ -106,51 +98,54 @@ function getRate() {
 
   for (let index3 = 0; index3 < getAllInputs.length; index3 += 1) {
     if (getAllInputs[index3].name === 'rate' && getAllInputs[index3].checked) {
-      getDiv6.innerHTML = `Avaliação: ${getAllInputs[index3].value}`
+      getDiv6.innerHTML = `Avaliação: ${getAllInputs[index3].value}`;
     }
   }
   return getDiv6;
 }
 
-//Ao clicar no botão de enviar, existe um texto no formato Observações: -Observações-
+// Ao clicar no botão de enviar, existe um texto no formato Observações: -Observações-
 function observation() {
   const getComment = document.getElementById('textarea').value;
   const getDiv7 = document.createElement('div');
-  getDiv7.innerHTML = `Observações: ${getComment}`
+  getDiv7.innerHTML = `Observações: ${getComment}`;
   return getDiv7;
 }
 
 function copyDatas() {
   sendBtn.addEventListener('click', (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const newestForm = document.createElement('form');
-  newestForm.id = 'evaluation-form';
+    const newestForm = document.createElement('form');
+    newestForm.id = 'evaluation-form';
 
-  const getDiv1 = nameAndLast();
-  newestForm.appendChild(getDiv1);
+    const getDiv1 = nameAndLast();
+    newestForm.appendChild(getDiv1);
   
-  const getDiv2 = emails();
-  newestForm.appendChild(getDiv2);
+    const getDiv2 = emails();
+    newestForm.appendChild(getDiv2);
 
-  const getDiv3 = houses();
-  newestForm.appendChild(getDiv3);
+    const getDiv3 = houses();
+    newestForm.appendChild(getDiv3);
 
-  const getDiv4 = families();
-  newestForm.appendChild(getDiv4);
+    const getDiv4 = families();
+    newestForm.appendChild(getDiv4);
 
-  const getDiv5 = familyBox();
-  newestForm.appendChild(getDiv5);
+    const getDiv5 = familyBox();
+    newestForm.appendChild(getDiv5);
 
-  const getDiv6 = getRate();
-  newestForm.appendChild(getDiv6);
+    const getDiv6 = getRate();
+    newestForm.appendChild(getDiv6);
 
-  const getDiv7 = observation();
-  newestForm.appendChild(getDiv7);
+    const getDiv7 = observation();
+    newestForm.appendChild(getDiv7);
 
-  mainForm.replaceChild(newestForm, forms);
-});
+    mainForm.replaceChild(newestForm, forms);
+  });
 }
 copyDatas();
 
 
+function submitData(e) {
+  e.preventDefault
+}
