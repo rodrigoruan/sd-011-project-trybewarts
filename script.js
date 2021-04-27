818e8b0fc7bd2bf517fc464493cae853956e720f
Diego Figueirêdo <8kball@gmail.com>
2021-04-27 14:35:08 -0300

    Agora vai :D

--- script.js
+++ script.js
@@ -35,34 +35,33 @@ function counterCharacter() {
 }
 textAreaId.addEventListener('keyup', counterCharacter);
 
+function getEls(selector) {
+  return document.querySelectorAll(selector);
+}
+
 // I'm about to lose my sanity so everything down here just
 // became pure spaghetti-gambiarra code. In advance I would
 // like to apologize to God and to everyone who comes to see
 // these abominations.
 // Sincerely, Diego.
-function renderFormsData(userInfo, technologies) {
+function renderFormsData() {
   const dataLabel = ['Nome', 'Email', 'Casa', 'Família', 'Matérias', 'Avaliação', 'Observações'];
+  const userInfo = [
+    `${getEls('#input-name')[0].value} ${getEls('#input-lastname')[0].value}`,
+    getEls('#input-email')[0].value,
+    getEls('#house')[0].value,
+    getEls('input[name="family"]')[0].value,
+    [...getEls('.subject:checked')].map((v) => v.value).join(', '),
+    getEls('input[name="rate"]')[0].value,
+    getEls('textarea')[0].value,
+  ];
   const listElement = document.createElement('ul');
-  const temp = userInfo[userInfo.length - 1];
-  userInfo[userInfo.length - 1] = technologies.join(', ');
-  userInfo.push(temp);
-  console.log(userInfo);
-  for (let i = 0; i < 7; i += 1) {
+  userInfo.forEach((info, i) => {
     const li = document.createElement('li');
-    li.innerText = `${dataLabel[i]}: ${userInfo[i] || 'Empty'}`;
+    li.innerText = `${dataLabel[i]}: ${info}`;
     listElement.appendChild(li);
-  }
+  });
   forms.appendChild(listElement);
 }
 
-function parseFormsData() {
-  const formsData = new FormData(forms);
-  const [userInfo, technologies] = [...formsData.entries()].reduce((acc, [field, value]) => {
-    if (field === 'lastname') acc[0][0] += ` ${value}`;
-    else acc[field === 'technology' ? 1 : 0].push(value);
-    return acc;
-  }, [[], []]);
-  renderFormsData(userInfo, technologies);
-}
-
-submitButton.addEventListener('click', parseFormsData);
+submitButton.addEventListener('click', renderFormsData);
