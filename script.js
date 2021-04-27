1d7e2fb470c921e93d3225663110473f5a5408ed
igordmf <igordanielmf@gmail.com>
2021-04-27 13:03:49 -0300

    Adiciona requisito 21

--- script.js
+++ script.js
@@ -34,11 +34,34 @@ function counterCharacters() {
 textArea.addEventListener('input', counterCharacters);
 
 // requisito 21
+function buildTextDiv1(fName, lName, email, house) {
+  const pNome = document.createElement('p')
+  pNome.innerText = `Nome: ${fName} ${lName}`;
+  const pEmail = document.createElement('p');
+  pEmail.innerText = `Email: ${email}`;
+  const pHouse = document.createElement('p');
+  pHouse.innerText = `Casa: ${house}`;
+  const form = document.getElementById('evaluation-form');
+  form.insertBefore(pHouse, form.firstChild);
+  form.insertBefore(pEmail, form.firstChild);
+  form.insertBefore(pNome, form.firstChild);
+}
+
 function getInfoDiv1() {
   const fName = document.getElementById('input-name').value;
   const lName = document.getElementById('input-lastname').value;
   const email = document.getElementById('input-email').value;
   const house = document.getElementById('house').value;
+  buildTextDiv1(fName, lName, email, house);
+}
+function buildTextDiv2(selectedFamily, subjectChecks) {
+  const pFamily = document.createElement('p');
+  pFamily.innerText = `Família: ${selectedFamily}`;
+  const pSubject = document.createElement('p');
+  pSubject.innerText = `Matérias: ${subjectChecks}`;
+  const form = document.getElementById('evaluation-form');
+  form.insertBefore(pSubject, form.firstChild);
+  form.insertBefore(pFamily, form.firstChild);
 }
 function getInfoDiv2() {
   // identificando a familia
@@ -52,6 +75,18 @@ function getInfoDiv2() {
   const subjectArray = Array.from(subject);
   let subjectChecks = subjectArray.filter((elemento) => elemento.checked === true);
   subjectChecks = subjectChecks.map((elemento) => elemento.value);
+  subjectChecks = subjectChecks.join(', ');
+  console.log(subjectChecks);
+  buildTextDiv2(selectedFamily, subjectChecks);
+}
+function biuldTextDiv3(rateValue, notes) {
+  const pRate = document.createElement('p');
+  pRate.innerText = `Avaliação: ${rateValue}`;
+  const pNotes = document.createElement('p');
+  pNotes.innerText = `Observações: ${notes}`;
+  const form = document.getElementById('evaluation-form');
+  form.insertBefore(pNotes, form.firstChild);
+  form.insertBefore(pRate, form.firstChild);
 }
 function getInfoDiv3() {
   // identificando a nota
@@ -62,11 +97,14 @@ function getInfoDiv3() {
 
   // identificando o comentário
   const notes = document.getElementById('textarea').value;
+  biuldTextDiv3(rateValue, notes);
 }
 function changeForms(event) {
   event.preventDefault();
-  getInfoDiv1();
-  getInfoDiv2();
   getInfoDiv3();
+  getInfoDiv2();
+  getInfoDiv1();
+  const divForms = document.querySelector('.div-form');
+  divForms.style.display = 'none';
 }
 submitBtn.addEventListener('click', changeForms);
