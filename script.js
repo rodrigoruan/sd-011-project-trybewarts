6113cf0651d4dbd5ac565a7be1a73ad55ade8773
DADA <davimarque2000@gmail.com>
2021-04-28 16:05:28 -0300

    adiciona contador, requisito 20

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const loginName = document.querySelector('#login');
 const loginsPass = document.querySelector('#passworld');
 const agree = document.querySelector('#agreement');
 const sbtButton = document.querySelector('#submit-btn');
+const textArea = document.querySelector('#textarea');
+const counterSpan = document.querySelector('#counter');
+let textCounter = 500;
 window.onload = () => {
   sbtButton.disabled = true;
+  counterSpan.innerText = textCounter;
 };
+textArea.addEventListener('keyup', () => {
+  textCounter = 500 - textArea.value.length;
+  counterSpan.innerText = textCounter;
+});
 loginButton.addEventListener('click', () => {
   if (loginName.value === 'tryber@teste.com' && loginsPass.value === '123456') {
     alert('Olá, Tryber!');
