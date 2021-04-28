6359e7795521643705d3db22eea5d955ee9af928
MIkaela Braga <bragamikaela@gmail.com>
2021-04-28 02:29:10 -0300

    REQUISITO 21 FINALIZADO COM LINT REVISADO

--- script.js
+++ script.js
@@ -39,36 +39,67 @@ function textCounter() {
 }
 textCounter();
 
-
-
-let object = {
-  Nome: document.getElementById('input-name').value,
-  Sobrenome: document.getElementById('input-lastname').value,
-  Email: document.getElementById('input-email').value,
-  Casa: document.getElementById('house').value,
-  Familia: document.getElementById('familia').value,
-  Matérias: document.getElementById('label-content').value,
-  Avaliação: document.getElementById('label-rate').value,
-  Observações: document.getElementById('textarea').value
-};
-
-
 const getButton = document.getElementById('submit-btn');
+const getForm = document.getElementById('evaluation-form');
+const getDivForm = document.getElementById('form');
+const creatDiv = document.createElement('div');
+getForm.appendChild(creatDiv);
 
-getButton.addEventListener('click', () => {
-  const getMain = document.querySelector('main');
-  const creatDiv = document.createElement('div');
-  getMain.appendChild(creatDiv);
-  const getForm = document.getElementById('evaluation-form');
-
-  for (let index = 0; index < object.length; index += 1) {
-
+function createName() {
+  const nome = document.getElementById('input-name');
+  const sobrenome = document.getElementById('input-lastname');
+  const nameParag = document.createElement('p');
+  creatDiv.appendChild(nameParag);
+  const fullName = `Nome: ${nome.value} ${sobrenome.value}`;
+  nameParag.innerText = fullName;
+}
+function createEmail() {
+  const email = document.getElementById('input-email').value;
+  const emailParag = document.createElement('p');
+  creatDiv.appendChild(emailParag);
+  emailParag.innerText = `Email: ${email}`;
+}
+function createHouse() {
+  const casa = document.getElementById('house').value;
+  const casaParag = document.createElement('p');
+  creatDiv.appendChild(casaParag);
+  casaParag.innerText = `Casa: ${casa}`;
+}
+function createFamily() {
+  const family = document.querySelector('[name=family]:checked').value;
+  const familyParag = document.createElement('p');
+  creatDiv.appendChild(familyParag);
+  familyParag.innerText = `Família: ${family}`;
+}
+function createMaterials() {
+  const materias = document.querySelectorAll('[class="subject"]:checked');
+  const materiaSelected = [];
+  for (let index = 0; index < materias.length; index += 1) {
+    materiaSelected.push(` ${materias[index].value}`);
   }
-  // getForm.remove();
+  const materiasParag = document.createElement('p');
+  creatDiv.appendChild(materiasParag);
+  materiasParag.innerText = `Matérias: ${materiaSelected}`;
+}
+function createEvaluation() {
+  const avaliacao = document.querySelector('[name=rate]:checked').value;
+  const avaliacaoParag = document.createElement('p');
+  creatDiv.appendChild(avaliacaoParag);
+  avaliacaoParag.innerText = `Avaliação: ${avaliacao}`;
+}
+function createObs() {
+  const observacoes = document.getElementById('textarea').value;
+  const obsParag = document.createElement('p');
+  creatDiv.appendChild(obsParag);
+  obsParag.innerText = `Observações: ${observacoes}`;
+}
+getButton.addEventListener('click', () => {
+  createName();
+  createEmail();
+  createHouse();
+  createFamily();
+  createMaterials();
+  createEvaluation();
+  createObs();
+  getDivForm.remove();
 });
-
-let test = {
-  Nome: 'Trybe',
-  Sobrenome: 'Xablau',
-};
-console.log(Object.values(test));
