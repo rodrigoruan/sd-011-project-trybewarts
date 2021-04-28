035c36fe354ae55370f5f4e1333791ddf0695202
DADA <davimarque2000@gmail.com>
2021-04-28 15:17:02 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const loginButton = document.querySelector('#submit');
 const loginName = document.querySelector('#login');
 const loginsPass = document.querySelector('#passworld');
+const agree = document.querySelector('#agreement');
+const sbtButton = document.querySelector('#submit-btn');
+window.onload = () => {
+  sbtButton.disabled = true;
+};
 loginButton.addEventListener('click', () => {
   if (loginName.value === 'tryber@teste.com' && loginsPass.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+agree.addEventListener('change', () => {
+  if (agree.checked === true) {
+    sbtButton.disabled = false;
+  } else {
+    sbtButton.disabled = true;
   }
 });
