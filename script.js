8b42251eb579a6c565246e1a2f1112f550a9a9c0
murilodospacos <murilopacos@gmail.com>
2021-04-28 13:21:22 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ function textAreaFunction() {
   counter.innerText = textArea.maxLength - textArea.value.length;
 }
 textArea.addEventListener('keyup', textAreaFunction);
+
+// Requisito 21 
+const form = document.getElementById('evaluation-form')
+const resultForms = document.querySelector('.result-form');
+
+function checkForm(event) {
+  event.preventDefault();
+
+  const inputName = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house').value;
+  const family = document.querySelector('#family-content').value;
+  const content = document.querySelector('#content').value;
+  const rate = document.querySelector('#rate').value;
+  const coment = document.querySelector('#textarea').value;
+
+  // Name
+  const paragraph = document.createElement('p');
+  paragraph.innerText = inputName;
+  resultForms.appendChild(paragraph);
+
+  // Sobre-Nome
+  const paragraph1 = document.createElement('p');
+  paragraph1.innerText = lastName;
+  resultForms.appendChild(paragraph1);
+  
+
+  // Email
+  const paragraph2 = document.createElement('p');
+  paragraph2.innerText = email;
+  resultForms.appendChild(paragraph2);
+
+}
+submitBtn.addEventListener('click', checkForm);
