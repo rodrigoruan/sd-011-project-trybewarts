74baf84496101dc3f3004cfb9bd4b917e86fb1ff
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-28 06:34:15 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -32,3 +32,71 @@ fagree.addEventListener('change', (e) => {
     fsubmitButton.disabled = true;
   }
 });
+
+// requisito 21
+
+const evaluationForms = document.getElementById('evaluation-form');
+const inputName = document.getElementById('input-name');
+const inputLastname = document.getElementById('input-lastname');
+const inputEmail = document.getElementById('input-email');
+const inputHouse = document.getElementById('house');
+const family = document.getElementsByClassName('family');
+const subject = document.getElementsByClassName('subject');
+const radio = document.getElementsByClassName('radioRate');
+const areaText = document.querySelector('#textarea');
+
+const subjectFunction = () => {
+  const array = [];
+  for (let i = 0; i < subject.length; i += 1) {
+    if (subject[i].checked === true) {
+      array.push(subject[i].value);
+    }
+  }
+  return array.join(', ');
+};
+
+const rateFunction = () => {
+  for (let i = 0; i < radio.length; i += 1) {
+    if (radio[i].checked === true) {
+      return radio[i];
+    }
+  }
+};
+
+const familyFunction = () => {
+  for (let index = 0; index < family.length; index += 1) {
+    if (family[index].checked === true) {
+      return family[index];
+    }
+  }
+};
+
+const paragraphMake = (list) => {
+  for (let i = 0; i < list.length; i += 1) {
+    const p = document.createElement('p');
+    p.innerText = list[i];
+    evaluationForms.appendChild(p);
+    const first = evaluationForms.firstChild;
+    evaluationForms.insertBefore(first, p);
+  }
+};
+
+fsubmitButton.addEventListener('click', (event) => {
+  const checked = familyFunction();
+  const subjects = subjectFunction();
+  const rated = rateFunction();
+
+  event.preventDefault();
+
+  // document.querySelector('.divResult').style.display = 'none';
+  const listBtn = [
+    `Nome: ${inputName.value} ${inputLastname.value}`,
+    `Email: ${inputEmail.value}`,
+    `Casa: ${inputHouse.value}`,
+    `Família: ${checked.value}`,
+    `Matérias: ${subjects}`,
+    `Avaliação: ${rated.value}`,
+    `Observações: ${areaText.value}`,
+  ];
+  paragraphMake(listBtn);
+});
