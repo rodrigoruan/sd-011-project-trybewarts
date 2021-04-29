e5e6a621671f9cfdecd8a59e6399900481711dde
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-28 23:01:49 -0300

    Adiciona requisito 21

--- script.js
+++ script.js
@@ -26,3 +26,39 @@ const maxlength = textArea.getAttribute('maxlength');
 textArea.addEventListener('keyup', () => {
   counter.innerText = maxlength - textArea.value.length;
 });
+
+// Requisito 21 contou com o auxílio de Gisele Costa
+function checkSubject() {
+  const subject = document.querySelectorAll('.subject:checked');
+  const list = [];
+
+  for (let index = 0; index < subject.length; index += 1) {
+    list.push(` ${subject[index].value}`);
+  }
+  return list;
+}
+
+const names = document.querySelector('#input-name');
+const lastName = document.querySelector('#input-lastname');
+const email = document.querySelector('#input-email');
+const house = document.querySelector('#house');
+const evaluationForm = document.querySelector('#evaluation-form');
+evaluationForm.addEventListener('submit', (event) => {
+  const allInputs = {
+    Nome: `${names.value} ${lastName.value}`,
+    Email: `${email.value}`,
+    Casa: `${house.value}`,
+    Família: `${document.querySelector('input[name="family"]:checked').value}`,
+    Matérias: `${checkSubject()}`,
+    Avaliação: `${document.querySelector('input[name="rate"]:checked').value}`,
+    Observações: `${document.querySelector('#textarea').value}`,
+  };
+
+  let newObject = {};
+  for (const index of Object.keys(allInputs)) {
+    newObject += `${index}: ${allInputs[index]}<br>`;
+  }
+
+  evaluationForm.innerHTML = newObject;
+  event.preventDefault();
+});
