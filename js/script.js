66ccab520c1485f0e6ff254f9c97dc0a262c19e7
heyset <c.inaciomatheus@gmail.com>
2021-04-29 14:40:25 -0300

    Agora sim add o requisito 21. Antes era o 20

--- js/script.js
+++ js/script.js
@@ -32,10 +32,65 @@ const userCommentaryInput = document.getElementById('textarea');
 const maxLength = 500;
 
 function updateCharacterCount() {
-  console.log(userCommentaryInput.value);
   const charsLeft = maxLength - userCommentaryInput.value.length;
   characterCounter.innerText = charsLeft;
 }
 
 userCommentaryInput.addEventListener('keyup', updateCharacterCount);
 userCommentaryInput.addEventListener('change', updateCharacterCount);
+
+const evaluationForm = document.getElementById('evaluation-form');
+const formAnswerElement = document.getElementById('form-answer');
+
+function getFormAnswer(formElement) {
+  const formData = new FormData(formElement);
+  const formAnswer = { skill: [] };
+
+  for (const pair of formData.entries()) {
+    const key = pair[0];
+    const value = pair[1];
+
+    if (key === 'skill') {
+      formAnswer[key].push(value);
+    } else {
+      formAnswer[key] = value;
+    }
+  }
+
+  return formAnswer;
+}
+
+function fillAnswers(answerObject) {
+  const keys = Object.keys(answerObject);
+  for (const key of keys) {
+    formAnswerElement.querySelector(`#answer-${key}`).innerText = answerObject[key];
+  }
+}
+
+evaluationForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const answerObject = getFormAnswer(evaluationForm);
+
+  delete answerObject.agreement;
+  answerObject.name = `${answerObject.firstname} ${answerObject.lastname}`;
+  delete answerObject.firstname;
+  delete answerObject.lastname;
+
+  if (typeof answerObject.skill !== 'string') {
+    let newSkillString = '';
+
+    for (const skill of answerObject.skill) {
+      newSkillString += `${skill}, `;
+    }
+
+    answerObject.skill = newSkillString;
+  }
+
+  fillAnswers(answerObject);
+  formAnswerElement.classList.add('modal-active');
+});
+
+document.getElementById('close-modal').addEventListener('click', () => {
+  formAnswerElement.classList.remove('modal-active');
+});
