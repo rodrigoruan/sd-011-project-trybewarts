c38b4200746e2883777df85231685f9ab8d5ec5a
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-29 14:07:36 -0300

    finaliza o projeto com o exercicio 21

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-// const { formatters } = require("stylelint");
-
 const btnLogin = document.getElementById('btnLogar');
 const inputLogin = document.getElementById('login');
 const inptPassword = document.getElementById('password');
 const btnCheck = document.getElementById('agreement');
 const btnSub = document.getElementById('submit-btn');
-const main = document.getElementsByClassName('main-container');
 
-function LoginAlert() {
+const LoginAlert = () => {
   if (inputLogin.value === 'tryber@teste.com' && inptPassword.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
-}
+};
 
 btnLogin.addEventListener('click', LoginAlert);
 
 btnSub.disabled = true;
 
-function validateButton() {
+const validateButton = () => {
   if (btnCheck.checked === false) {
     btnSub.disabled = true;
   } else {
     btnSub.disabled = false;
   }
-}
+};
 
 btnCheck.addEventListener('click', validateButton);
 
-// tentando desenrolar o requisito 21!
-const forms = document.getElementById('evaluation-form');
-
+const evaluationForms = document.getElementById('evaluation-form');
 const nameInput = document.getElementById('input-name');
 const lastNameInput = document.getElementById('input-lastname');
 const emailInput = document.getElementById('input-email');
@@ -39,17 +34,8 @@ const houseInput = document.getElementById('house');
 const familyInput = document.getElementsByClassName('family');
 const subjectInput = document.getElementsByClassName('subject');
 const radioInput = document.getElementsByClassName('radioRate');
-
-// function createParagraphs(array) {
-//   for (let i = 0; i < array.length; i += 1) {
-//     const p = document.createElement('p');
-//     p.innerText = array[i];
-//     forms.appendChild(p);
-//   }
-// }
-// }
-
 const textArea = document.querySelector('#textarea');
+
 textArea.addEventListener('keyup', () => {
   const paragrafo = document.querySelector('#counter');
   let caracteresDisponiveis = 0;
@@ -64,8 +50,7 @@ const subject = () => {
       array.push(subjectInput[i].value);
     }
   }
-  const resultado = array.join(', ');
-  return resultado;
+  return array.join(', ');
 };
 
 const rate = () => {
@@ -84,37 +69,33 @@ const family = () => {
   }
 };
 
-const values = [];
-
-const criaParagrafo = (event) => {
-  for (let index = 0; index < event.length; index += 1) {
-    const mostraResultados = document.createElement('p');
-    mostraResultados.innerHTML = event[index];
-    main.appendChild(mostraResultados);
+const makeP = (list) => {
+  for (let i = 0; i < list.length; i += 1) {
+    const p = document.createElement('p');
+    p.innerText = list[i];
+    evaluationForms.appendChild(p);
+    const first = evaluationForms.firstChild;
+    evaluationForms.insertBefore(first, p);
   }
 };
 
 btnSub.addEventListener('click', (event) => {
-  const checkedFunction = family();
+  const checked = family();
   const subjects = subject();
   const rated = rate();
 
-  forms.style.display = 'none';
-
-  const list = {
-    Nome: nameInput.value,
-    Sobrenome: lastNameInput.value,
-    Email: emailInput.value,
-    Casa: houseInput.value,
-    Família: checkedFunction.value,
-    Matérias: subjects,
-    Avaliação: rated.value,
-    Observações: textArea.value,
-  };
-
   event.preventDefault();
-  values.push(list);
-  // document.forms[1].reset();
-  console.log(values);
-  criaParagrafo(list);
+
+  document.querySelector('.divResult').style.display = 'none';
+
+  const list = [
+    `Nome: ${nameInput.value} ${lastNameInput.value}`,
+    `Email: ${emailInput.value}`,
+    `Casa: ${houseInput.value}`,
+    `Família: ${checked.value}`,
+    `Matérias: ${subjects}`,
+    `Avaliação: ${rated.value}`,
+    `Observações: ${textArea.value}`,
+  ];
+  makeP(list);
 });
