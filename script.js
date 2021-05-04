fc5cc0848388cfa115d19512026f740359d744b1
nandovbr <nandovbr15@hotmail.com>
2021-05-04 15:09:57 -0300

    projeto tribewarts

--- script.js
+++ script.js
@@ -1,48 +1,36 @@
 const loginButton = document.getElementById('login-button');
 const loginInput = document.getElementById('login');
 const passwordInput = document.getElementById('senha');
-
 loginButton.addEventListener('click', () => {
   const loginValue = loginInput.value;
   const passwordValue = passwordInput.value;
-
   if (loginValue !== 'tryber@teste.com' && passwordValue !== '123456') {
     alert('Login ou senha inválidos.');
   } else {
     alert('Olá, Tryber!');
   }
 });
-
 const agreementButton = document.getElementById('agreement');
 const submitButton = document.getElementById('submit-btn');
-
 function disabledButton() {
   submitButton.disabled = true;
 }
-
 disabledButton();
-
 agreementButton.addEventListener('click', () => {
   const agreementStatus = agreementButton.checked;
-
   if (agreementStatus === true) {
     submitButton.disabled = false;
   }
-
   if (agreementStatus === false) {
     disabledButton();
   }
 });
-
 const msgElement = document.getElementById('textarea');
 const cntElement = document.getElementById('counter');
-
 msgElement.addEventListener('input', (e) => {
   const { target } = e;
-
   const maxLength = target.getAttribute('maxlength');
   const currentLength = target.value.length;
-
   cntElement.innerHTML = `${maxLength - currentLength}`;
 });
 
